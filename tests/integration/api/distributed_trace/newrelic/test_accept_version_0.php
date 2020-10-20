816d91781e413c96d3637f50f000ebb6fcdf2ff9
Johannes Tax <jtax@newrelic.com>
2020-10-20 14:06:01 -0700

    Fix ids

--- tests/integration/api/distributed_trace/newrelic/test_accept_version_0.php
+++ tests/integration/api/distributed_trace/newrelic/test_accept_version_0.php
@@ -18,5 +18,5 @@ ok - Rejected
 
 require_once(realpath (dirname ( __FILE__ )) . '/../../../../include/tap.php');
 
-$payload = "{\"v\":[0,0],\"d\":{\"ty\":\"App\",\"ac\":\"333333\",\"ap\":\"4444444\",\"id\":\"332c7b9a18777990\",\"tr\":\"332c7b9a18777990\",\"pr\":1.28674,\"sa\":true,\"ti\":1530311294670}}";
+$payload = "{\"v\":[0,0],\"d\":{\"ty\":\"App\",\"ac\":\"{$_ENV['ACCOUNT_supportability_trusted']}\",\"ap\":\"4444444\",\"id\":\"332c7b9a18777990\",\"tr\":\"332c7b9a18777990\",\"pr\":1.28674,\"sa\":true,\"ti\":1530311294670}}";
 $result = tap_equal(true, newrelic_accept_distributed_trace_payload($payload), 'Rejected');
