13607005f0c499672f482d63fe7f7852131e3315
Johannes Tax <jtax@newrelic.com>
2020-10-20 09:31:41 -0700

    Use dummy account and application ids

--- tests/integration/api/distributed_trace/newrelic/test_accept_version_1.php
+++ tests/integration/api/distributed_trace/newrelic/test_accept_version_1.php
@@ -8,13 +8,6 @@
 Test that a version 0.1 payload is accepted.
 */
 
-/*SKIPIF
-<?php
-if (!$_ENV["ACCOUNT_distributed_traces"] || !$_ENV["APP_distributed_traces"]) {
-    die("skip: env vars required");
-}
-*/
-
 /*INI
 newrelic.distributed_tracing_enabled=1
 */
@@ -25,6 +18,6 @@ ok - Accepted
 
 require_once(realpath (dirname ( __FILE__ )) . '/../../../../include/tap.php');
 
-$payload = "{\"v\":[0,1],\"d\":{\"ty\":\"App\",\"ac\":\"{$_ENV['ACCOUNT_distributed_traces']}\",\"ap\":\"{$_ENV['APP_distributed_traces']}\",\"id\":\"332c7b9a18777990\",\"tr\":\"332c7b9a18777990\",\"pr\":1.28674,\"sa\":true,\"ti\":1530311294670}}";
+$payload = "{\"v\":[0,1],\"d\":{\"ty\":\"App\",\"ac\":\"333333\",\"ap\":\"4444444\",\"id\":\"332c7b9a18777990\",\"tr\":\"332c7b9a18777990\",\"pr\":1.28674,\"sa\":true,\"ti\":1530311294670}}";
 
 $result = tap_equal(true, newrelic_accept_distributed_trace_payload($payload), 'Accepted');
