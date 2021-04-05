4c3ccd5ebc6a84e9fca32714cb3973bceb4c0348
Joshua Benuck <jbenuck@newrelic.com>
2021-04-05 17:25:42 -0500

    add back in 5.3 and 5.4, for now

--- make/release.mk
+++ make/release.mk
@@ -91,6 +91,8 @@ endif
 	$(MAKE) agent-clean; $(MAKE) release-7.0-no-zts
 	$(MAKE) agent-clean; $(MAKE) release-5.6-no-zts
 	$(MAKE) agent-clean; $(MAKE) release-5.5-no-zts
+	$(MAKE) agent-clean; $(MAKE) release-5.4-no-zts
+	$(MAKE) agent-clean; $(MAKE) release-5.3-no-zts
 ifeq (yes,$(BUILD_PHP_80))
 	$(MAKE) agent-clean; $(MAKE) release-8.0-zts
 endif
@@ -101,6 +103,8 @@ endif
 	$(MAKE) agent-clean; $(MAKE) release-7.0-zts
 	$(MAKE) agent-clean; $(MAKE) release-5.6-zts
 	$(MAKE) agent-clean; $(MAKE) release-5.5-zts
+	$(MAKE) agent-clean; $(MAKE) release-5.4-zts
+	$(MAKE) agent-clean; $(MAKE) release-5.3-zts
 
 #
 # Add a new target to build the agent against build machines.
@@ -150,6 +154,8 @@ $(eval $(call RELEASE_AGENT_TARGET,7.1,20160303))
 $(eval $(call RELEASE_AGENT_TARGET,7.0,20151012))
 $(eval $(call RELEASE_AGENT_TARGET,5.6,20131226))
 $(eval $(call RELEASE_AGENT_TARGET,5.5,20121212))
+$(eval $(call RELEASE_AGENT_TARGET,5.4,20100525))
+$(eval $(call RELEASE_AGENT_TARGET,5.3,20090626))
 
 #
 # Release directories
