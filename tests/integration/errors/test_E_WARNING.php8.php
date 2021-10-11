2db515c58f1a0715867966447f030a7aa2067031
Michael Fulbright <mfulbright@newrelic.com>
2021-10-11 15:22:37 -0400

    Fixes failing tests in tests/integrations/error

--- tests/integration/errors/test_E_WARNING.php8.php
+++ tests/integration/errors/test_E_WARNING.php8.php
@@ -31,8 +31,7 @@ log_errors=0
     [
       "?? when",
       "OtherTransaction/php__FILE__",
-      "include(): Failed opening 'abc.php' for inclusion (include_path='.:')",
-      "E_WARNING",
+      "/include\\(\\): Failed opening 'abc.php' for inclusion \\(include_path='.:.*'\\)/",      "E_WARNING",
       {
         "stack_trace": "??",
         "agentAttributes": "??",
@@ -56,7 +55,7 @@ log_errors=0
         "type": "TransactionError",
         "timestamp": "??",
         "error.class": "E_WARNING",
-        "error.message": "include(): Failed opening 'abc.php' for inclusion (include_path='.:')",
+        "error.message": "/include\\(\\): Failed opening 'abc.php' for inclusion \\(include_path='.:.*'\\)/",
         "transactionName": "OtherTransaction\/php__FILE__",
         "duration": "??",
         "nr.transactionGuid": "??"
