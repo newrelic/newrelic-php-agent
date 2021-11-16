e394e5a1649e81456a3ce731b8fe3f1f5c704343
Zach Neumann <zneumann@newrelic.com>
2021-11-16 15:34:59 -0500

    add colored output; change ok/not ok to pass/fail

--- src/integration_runner/reports.go
+++ src/integration_runner/reports.go
@@ -14,11 +14,36 @@ import (
 	"newrelic/integration"
 )
 
+var (
+  Black   = Color("\033[1;30m%s\033[0m")
+  Red     = Color("\033[1;31m%s\033[0m")
+  Green   = Color("\033[1;32m%s\033[0m")
+  Yellow  = Color("\033[1;33m%s\033[0m")
+  Purple  = Color("\033[1;34m%s\033[0m")
+  Magenta = Color("\033[1;35m%s\033[0m")
+  Teal    = Color("\033[1;36m%s\033[0m")
+  White   = Color("\033[1;37m%s\033[0m")
+)
+
+var (
+  Good = Green
+  Warn = Yellow
+  Fata = Red
+)
+
 type TestRunTotals struct {
 	passed  int
 	skipped int
 	failed  int
 	xfail   int
+}
+
+func Color(colorString string) func(...interface{}) string {
+  sprint := func(args ...interface{}) string {
+    return fmt.Sprintf(colorString,
+      fmt.Sprint(args...))
+  }
+  return sprint
 }
 
 func (totals *TestRunTotals) Accumulate(test *integration.Test) {
@@ -45,27 +70,28 @@ func tapOutput(tests []*integration.Test) {
 
 		switch {
 		case test.Skipped:
-			fmt.Println("skip -", name, "#", test.Err)
+			fmt.Println(Warn("skip -"), name, "#", Warn(test.Err))
 		case test.Failed:
 			if "" != test.Xfail {
 				fmt.Println("xfail -", name)
 			} else {
 				if test.Err != nil {
-					fmt.Println("not ok -", name, "#", test.Err)
+					fmt.Println(Fata("FAIL -"), Fata(name), "#", test.Err)
 					if len(test.Output) > 0 {
 						fmt.Println()
 						io.Copy(os.Stdout, bytes.NewReader(test.Output))
 						fmt.Println()
 					}
 				} else {
-					fmt.Println("not ok -", name)
+					fmt.Println(Fata("FAIL -"), Fata(name))
 					for _, e := range test.Failures {
 						fmt.Println(e)
 					}
 				}
 			}
 		default:
-			fmt.Printf("ok - %s", name)
+			fmt.Printf(Good("pass - "))
+			fmt.Printf("%s", name)
 			if test.Duration > 0 {
 				fmt.Printf(" # time=%vs", test.Duration.Seconds())
 			}
@@ -74,6 +100,10 @@ func tapOutput(tests []*integration.Test) {
 	}
 	fmt.Println("#", totals.passed, "passed")
 	fmt.Println("#", totals.skipped, "skipped")
-	fmt.Println("#", totals.failed, "failed")
+    if totals.failed == 0 {
+	    fmt.Println("#", Good(totals.failed), Good("failed"))
+    } else {
+	    fmt.Println("#", Fata(totals.failed), Fata("failed"))
+    }
 	fmt.Println("#", totals.xfail, "xfail")
 }
