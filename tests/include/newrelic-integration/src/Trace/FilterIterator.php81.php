29cf7d8cccb42f976c0755f5cd5fd4a0d65a5953
Zach Neumann <zneumann@newrelic.com>
2022-01-10 15:28:08 -0700

    change tests to define return type when extending for php 8.1

---
+++ tests/include/newrelic-integration/src/Trace/FilterIterator.php81.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+namespace NewRelic\Integration\Trace;
+
+use FilterIterator as BaseFilterIterator;
+use Iterator;
+
+/**
+ * An extended version of the default FilterIterator that ships with PHP that
+ * allows the use of a filter callback without requiring the user to extend the
+ * FilterIterator class directly.
+ */
+class FilterIterator extends BaseFilterIterator
+{
+    /**
+     * The filter callback.
+     *
+     * @var callable
+     */
+    protected $filter;
+
+    /**
+     * Constructs a new filter iterator.
+     *
+     * @param Iterator $it     The iterator that is to be filtered.
+     * @param callable $filter The callback that, given an element within the
+     *                         iterator, either returns true to retain it or
+     *                         false to discard it.
+     */
+    public function __construct(Iterator $it, $filter)
+    {
+        parent::__construct($it);
+        $this->filter = $filter;
+    }
+
+    /**
+     * The `accept()` implementation that delegates to the filter callback.
+     *
+     * @return boolean
+     */
+    public function accept(): bool
+    {
+        $filter = $this->filter;
+        return $filter($this->current());
+    }
+}
