fa7ec776c57d594dfcf6892468764cf843ea4b8c
Zach Neumann <zneumann@newrelic.com>
2022-01-10 15:28:08 -0700

    change tests to define return type when extending for php 8.1

--- tests/include/integration.php
+++ tests/include/integration.php
@@ -8,16 +8,31 @@
  * Autoload the newrelic/integration library.
  */
 
-$files = array(
-  'DatastoreInstance.php',
-  'Metric.php',
-  'SlowSQL.php',
-  'Trace/Segment.php',
-  'Trace/FilterIterator.php',
-  'Trace/StringTable.php',
-  'Trace.php',
-  'Transaction.php',
-);
+if (version_compare(PHP_VERSION, "8.1", "<")) {
+  $files = array(
+    'DatastoreInstance.php',
+    'Metric.php',
+    'SlowSQL.php',
+    'Trace/Segment.php',
+    'Trace/FilterIterator.php',
+    'Trace/StringTable.php',
+    'Trace.php',
+    'Transaction.php',
+  );
+}
+else
+{
+  $files = array(
+    'DatastoreInstance.php',
+    'Metric.php',
+    'SlowSQL.php',
+    'Trace/Segment.php81.php',
+    'Trace/FilterIterator.php81.php',
+    'Trace/StringTable.php',
+    'Trace.php',
+    'Transaction.php',
+  );
+}
 
 foreach ($files as $file) {
   require_once __DIR__.'/newrelic-integration/src/'.$file;
