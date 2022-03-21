613f20b6a2b4ed4c71b61826e0e3a6bf8457e088
Michal Nowacki <mnowacki@newrelic.com>
2022-03-21 14:59:42 -0400

    tests(integration): Fix integration tests
    Use value of `newrelic.distributed_tracing_enabled` from test's `/*INI`
    directive in integration_runner's mock external service to correctly
    handle tests expectations. With distributed tracing enabled by default,
    integration_runner's mock external service has distributed tracing
    enabled regardless of the value set in test's `/*INI` directive and
    didn't handle CAT headers. With this change it is possible to control
    distributed tracing setting in mock external service and it gets disabled
    if integration test demands it by explicit setting in `/*INI` directive.

--- src/integration_runner/main.go
+++ src/integration_runner/main.go
@@ -170,6 +170,7 @@ func merge(a, b map[string]string) map[string]string {
 
 func catRequest(w http.ResponseWriter, r *http.Request) {
 	catFile := r.URL.Query().Get("file")
+	dtEnabled := r.URL.Query().Get("dt_enabled");
 	if "" == catFile {
 		http.Error(w, "cat failure: no file provided", http.StatusBadRequest)
 		return
@@ -178,6 +179,9 @@ func catRequest(w http.ResponseWriter, r *http.Request) {
 	env := merge(ctx.Env, nil)
 	settings := merge(ctx.Settings, nil)
 	settings["newrelic.appname"] = "ignore"
+	if ("false" == dtEnabled) {
+	    settings["newrelic.distributed_tracing_enabled"] = "false";
+	}
 
 	tx, err := integration.CgiTx(integration.ScriptFile(catFile), env, settings, r.Header, ctx)
 	if nil != err {
