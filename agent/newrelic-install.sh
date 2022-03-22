fb2273ea82f05eee4b94005adb9a41c1c8b5b2e1
Michal Nowacki <mnowacki@newrelic.com>
2022-03-22 17:26:42 -0400

    fix(agent/scripts): Fix detecting Linux distribution
    Use `/etc/os-release` instead of `/etc/dpkg` to detect if running on
    Debian-based distribution. Using `/etc/dpkg` may cause issues if `dpkg`
    package is installed on non-Debian distribution (like Alpine).

--- agent/newrelic-install.sh
+++ agent/newrelic-install.sh
@@ -120,11 +120,9 @@ error() {
 # Get the OS type.
 #
 ostype=
-if [ -f /proc/version ] && [ -n "$(grep 'Alpine' /proc/version)" ]; then
-  ostype=alpine
-elif [ -f /etc/redhat-release -o -f /etc/redhat_version ]; then
+if [ -f /etc/redhat-release -o -f /etc/redhat_version ]; then
   ostype=rhel
-elif [ -d /etc/dpkg ]; then
+elif [ -f /etc/os-release ] && grep -q -i 'Debian' /etc/os-release; then
   ostype=debian
 elif [ -f /etc/alpine-release ] || [ -d /etc/apk ]; then
   ostype=alpine
