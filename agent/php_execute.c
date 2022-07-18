5faca7fc2208cd853d594a5586084af20049428f
Michal Nowacki <mnowacki@newrelic.com>
2022-07-18 18:53:55 +0000

    feat(agent): detect Monolog library

--- agent/php_execute.c
+++ agent/php_execute.c
@@ -565,6 +565,9 @@ static nr_library_table_t libraries[] = {
 
     {"Xoops", "class/xoopsload.php", NULL},
     {"E107", "e107_handlers/e107_class.php", NULL},
+
+    /* Monolog - Logging for PHP */
+    {"Monolog", "monolog/logger.php", nr_monolog_enable}
 };
 
 static size_t num_libraries = sizeof(libraries) / sizeof(nr_library_table_t);
@@ -870,6 +873,8 @@ static void nr_php_execute_add_library_supportability_metric(
 static void nr_execute_handle_library(const char* filename TSRMLS_DC) {
   char* filename_lower = nr_string_to_lowercase(filename);
   size_t i;
+
+  // nrl_info(NRL_INSTRUMENT, "nr_execute_handle_library processing file: %s", filename);
 
   for (i = 0; i < num_libraries; i++) {
     if (nr_stridx(filename_lower, libraries[i].file_to_check) >= 0) {
