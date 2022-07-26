6714ad28e767f7d65f55d283344718d20c2f947f
Michal Nowacki <mnowacki@newrelic.com>
2022-07-26 20:23:23 +0000

    feat(agent): create log event message
    Combine second ($message) and third ($context, if it is not empty array)
    arguments of Monolog\Logger::addRecord to create log event's message. If
    $context is an empty array it will not be appended to the final message.

--- agent/lib_monolog.c
+++ agent/lib_monolog.c
@@ -97,6 +97,190 @@ static char* nr_monolog_get_level_name(zval* logger,
 }
 
 /*
+ * Purpose : Convert $message argument of Monolog\Logger::addRecord to a string
+ *
+ * Params  : Monolog\Logger::addRecord argument list
+ *
+ * Returns : A new string with Monolog's log message; caller must free
+ */
+
+static char* nr_monolog_get_message(NR_EXECUTE_PROTO TSRMLS_DC) {
+  char* message = NULL;
+  zval* message_arg = NULL;
+
+  message_arg = nr_php_arg_get(2, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
+  if (!nr_php_is_zval_valid_string(message_arg)) {
+    nrl_verbosedebug(NRL_INSTRUMENT,
+                     "%s: expected $message be a valid string, got type %d",
+                     __func__, Z_TYPE_P(message_arg));
+    message = nr_strdup("");
+  } else {
+    message = nr_strdup(Z_STRVAL_P(message_arg));
+  }
+  nr_php_arg_release(&message_arg);
+
+  return message;
+}
+
+/*
+ * Purpose : Format key of $context array's element as string
+ *
+ * Params  : zend_hash_key
+ * *
+ * Returns : A new string representing zval; caller must free
+ *
+ */
+static char* nr_monolog_fmt_context_key(zend_hash_key* hash_key) {
+  char* key_str = NULL;
+  zval* key = nr_php_zval_alloc();
+  if (nr_php_zend_hash_key_is_string(hash_key)) {
+    nr_php_zval_str(key, nr_php_zend_hash_key_string_value(hash_key));
+    key_str = nr_formatf("%s", Z_STRVAL_P(key));
+  } else if (nr_php_zend_hash_key_is_numeric(hash_key)) {
+    ZVAL_LONG(key, (zend_long)nr_php_zend_hash_key_integer(hash_key));
+    key_str = nr_formatf("%ld", (long)Z_LVAL_P(key));
+  } else {
+    /*
+     * This is a warning because this really, really shouldn't ever happen.
+     */
+    nrl_warning(NRL_INSTRUMENT, "%s: unexpected key type", __func__);
+    key_str = nr_formatf("unsupported-key-type");
+  }
+  nr_php_zval_free(&key);
+  return key_str;
+}
+
+/*
+ * Purpose : Format value of $context array's  element as string
+ *
+ * Params  : zval value
+ * *
+ * Returns : A new string representing zval; caller must free
+ *
+ */
+static char* nr_monolog_fmt_context_value(zval* zv) {
+  char* val_str = NULL;
+  zval* zv_str = nr_php_zval_alloc();
+
+  ZVAL_DUP(zv_str, zv);
+  convert_to_string(zv_str);
+  val_str = nr_strdup(Z_STRVAL_P(zv_str));
+  nr_php_zval_free(&zv_str);
+
+  return val_str;
+}
+
+/*
+ * Purpose : Format an element of $context array as "key => value" string
+ *
+ * Params  : zval value, pointer to string buffer to store formatted output
+ * and hash key
+ *
+ * Side effect : string buffer is reallocated with each call.
+ *
+ * Returns : ZEND_HASH_APPLY_KEEP to keep iteration
+ *
+ */
+static int nr_monolog_fmt_context_item(zval* value,
+                                       char** strbuf,
+                                       zend_hash_key* hash_key TSRMLS_DC) {
+  NR_UNUSED_TSRMLS;
+  char* key = nr_monolog_fmt_context_key(hash_key);
+  char* val = nr_monolog_fmt_context_value(value);
+
+  char* kv_str = nr_formatf("%s => %s", key, val);
+  nr_free(key);
+  nr_free(val);
+
+  char* sep = nr_strlen(*strbuf) > 1 ? ", " : "";
+  *strbuf = nr_str_append(*strbuf, kv_str, sep);
+  nr_free(kv_str);
+
+  return ZEND_HASH_APPLY_KEEP;
+}
+
+/*
+ * Purpose : Iterate over $context array and format each element
+ *
+ * Params  : string buffer to store formatted output and
+ * Monolog\Logger::addRecord argument list
+ *
+ * Returns : A new string with Monolog's log context
+ */
+static char* nr_monolog_fmt_context(char* strbuf,
+                                    HashTable* context TSRMLS_DC) {
+  strbuf = nr_str_append(strbuf, "[", "");
+
+  nr_php_zend_hash_zval_apply(context,
+                              (nr_php_zval_apply_t)nr_monolog_fmt_context_item,
+                              (void*)&strbuf TSRMLS_CC);
+
+  return nr_str_append(strbuf, "]", "");
+}
+
+/*
+ * Purpose : Convert $context argument of Monolog\Logger::addRecord to a string
+ *
+ * Params  : # of Monolog\Logger::addRecord arguments, and
+ * Monolog\Logger::addRecord argument list
+ *
+ * Returns : A new string with Monolog's log context
+ */
+static char* nr_monolog_get_context(size_t argc, NR_EXECUTE_PROTO TSRMLS_DC) {
+  char* context = nr_strdup("");
+  zval* context_arg = NULL;
+
+  if (3 > argc) {
+    nrl_verbosedebug(NRL_INSTRUMENT, "%s: $context not available", __func__);
+    goto return_context;
+  }
+
+  context_arg = nr_php_arg_get(3, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
+
+  if (!nr_php_is_zval_valid_array(context_arg)) {
+    nrl_verbosedebug(NRL_INSTRUMENT,
+                     "%s: expected $context be a valid array, got type %d",
+                     __func__, Z_TYPE_P(context_arg));
+    goto return_context;
+  }
+
+  if (0 == nr_php_zend_hash_num_elements(Z_ARRVAL_P(context_arg))) {
+    nrl_verbosedebug(NRL_INSTRUMENT, "%s: $context has no elements", __func__);
+    goto return_context;
+  }
+
+  context = nr_monolog_fmt_context(context, Z_ARRVAL_P(context_arg) TSRMLS_CC);
+
+return_context:
+  nr_php_arg_release(&context_arg);
+  return context;
+}
+
+/*
+ * Purpose : Combine $message and $context arguments of
+ * Monolog\Logger::addRecord into a single string to be used as a message
+ * property of the log event.
+ *
+ * Params  : # of Monolog\Logger::addRecord arguments, and
+ * Monolog\Logger::addRecord argument list
+ *
+ * Returns : A new string with a log record message; caller must free
+ */
+static char* nr_monolog_build_message(size_t argc, NR_EXECUTE_PROTO TSRMLS_DC) {
+  char* message_and_context = nr_strdup("");
+
+  char* message = nr_monolog_get_message(NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
+  message_and_context = nr_str_append(message_and_context, message, "");
+  nr_free(message);
+
+  char* context = nr_monolog_get_context(argc, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
+  message_and_context = nr_str_append(message_and_context, context, " ");
+  nr_free(context);
+
+  return message_and_context;
+}
+
+/*
  * Purpose : Create timestamp for the log event by inspecting $datetime argument
  * of Monolog\Logger::addRecord
  *
@@ -149,18 +333,9 @@ NR_PHP_WRAPPER(nr_monolog_logger_addrecord) {
   char* level_name
       = nr_monolog_get_level_name(this_var, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
 
-  zval* message = nr_php_arg_get(2, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
   size_t argc = nr_php_get_user_func_arg_count(NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
-
-  /* Get values of optional arguments: $context and $datetime */
-  zval* context = NULL;
-
-  if (3 <= argc) {
-    context = nr_php_arg_get(3, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
-    nrl_verbosedebug(
-        NRL_INSTRUMENT, "%s: typeof(context)=[%d], len(context)=%zu", __func__,
-        Z_TYPE_P(context), nr_php_zend_hash_num_elements(Z_ARRVAL_P(context)));
-  }
+  char* message
+      = nr_monolog_build_message(argc, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
 
   nrtime_t timestamp
       = nr_monolog_get_timestamp(api, argc, NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
@@ -168,17 +343,15 @@ NR_PHP_WRAPPER(nr_monolog_logger_addrecord) {
   nrl_verbosedebug(NRL_INSTRUMENT,
                    "%s: #args = %zu, Monolog API: [%d], level=[%s], "
                    "message=[%s], timestamp=[%" PRIu64 "]",
-                   __func__, argc, api, level_name, Z_STRVAL_P(message),
-                   timestamp);
+                   __func__, argc, api, level_name, message, timestamp);
 
   /* construct the log_event from level, message, context and datetime */
 
   nr_free(level_name);
+  nr_free(message);
 
   NR_PHP_WRAPPER_CALL
-  if (context)
-    nr_php_arg_release(&context);
-  nr_php_arg_release(&message);
+
   nr_php_scope_release(&this_var);
 }
 NR_PHP_WRAPPER_END
