2c947195dd684f5e05fa23cac30b2e5a70a07db9
Michal Nowacki <mnowacki@newrelic.com>
2022-08-17 21:10:16 -0400

    pr-review: remove debug log message
    Avoid potential agent log flooding.

--- axiom/nr_txn.c
+++ axiom/nr_txn.c
@@ -3356,11 +3356,9 @@ static void nr_txn_add_log_event(nrtxn_t* txn,
 
   if (!nr_txn_log_forwarding_enabled(txn)) {
     return;
-  }
+    }
 
   if (nr_strempty(log_message)) {
-    nrl_debug(NRL_TXN, "%s: not forwarding log event with empty log message",
-              __func__);
     return;
   }
 
