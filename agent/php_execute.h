f04ea34b79cbb56d6bf95b6e1bfda852a728ca61
Amber Sistla <asistla@newrelic.com>
2022-08-23 14:36:42 -0700

    fix(agent): PR feedback comment clarification.

--- agent/php_execute.h
+++ agent/php_execute.h
@@ -70,11 +70,13 @@ extern void nr_php_user_instrumentation_from_opcache(TSRMLS_D);
 #if ZEND_MODULE_API_NO >= ZEND_8_0_X_API_NO /* PHP8+ */
 /*
  * Purpose : Call the necessary functions needed to instrument a function by
- *           starting a transaction for a function that has just started.  This
- *           function is registered via the Observer API and will be called by
- *           the zend engine every time a function begins.  The zend engine
- *           directly provides the zend_execute_data which has all details we
- *           need to know about the function.
+ *           starting a transaction or segment for a function that has just
+ * started.  This function is registered via the Observer API and will be called
+ * by the zend engine every time a function begins.  The zend engine directly
+ * provides the zend_execute_data which has all details we need to know about
+ * the function. This and nr_php_execute_observer_fcall_end sum to provide all
+the functionality of nr_php_execute and nr_php _execute_enabled and as such will
+use all the helper functions they also used.
  *
  *
  * Params  : 1. zend_execute_data: everything we need to know about the
@@ -85,12 +87,14 @@ extern void nr_php_user_instrumentation_from_opcache(TSRMLS_D);
 void nr_php_execute_observer_fcall_begin(zend_execute_data* execute_data);
 /*
  * Purpose : Call the necessary functions needed to instrument a function when
- *           ending a transaction for a function that has just ended.  This
- *           function is registered via the Observer API and will be called by
- *           the zend engine every time a function ends.  The zend engine
- *           directly provides the zend_execute_data and teh return_value
- *           pointer, both of which have all details that the agent needs to
- *           know about the function.
+ *           ending a transaction or segment for a function that has just ended.
+ * This function is registered via the Observer API and will be called by the
+ * zend engine every time a function ends.  The zend engine directly provides
+ * the zend_execute_data and the return_value pointer, both of which have all
+ * details that the agent needs to know about the function. This and
+ * nr_php_execute_observer_fcall_start sum to provide all the functionality of
+ * nr_php_execute and nr_php_execute_enabled and as such will use all the helper
+ * functions they also used.
  *
  *
  * Params  : 1. zend_execute_data: everything to know about the function.
