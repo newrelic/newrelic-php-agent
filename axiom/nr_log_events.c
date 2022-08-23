cb4f70828c6acc2ac2c6870ec44d9d78cedd053e
Michael Fulbright <mfulbright@newrelic.com>
2022-08-23 12:31:35 -0400

    feat(agent): Implement priority sampling for log events
    * Create nr_log_event_t which uses a minmax heap (like segments) and
       changes the implementation from nr_analytics_events.
    * Removes the nr_log_events_get_event_json() method as it was for
       retrieving an event by index from the array of analytic events.  A new
       mechanism for converting the minmax heap to a set like done for
       spans will be required (not part of this commit).

--- axiom/nr_log_events.c
+++ axiom/nr_log_events.c
@@ -8,32 +8,180 @@
 #include <math.h>
 #include <stdlib.h>
 
+#include "nr_limits.h"
 #include "nr_log_events.h"
-#include "nr_analytics_events_private.h"
+#include "nr_log_events_private.h"
+#include "nr_log_event_private.h"
 #include "util_memory.h"
+#include "util_minmax_heap.h"
 
-bool nr_log_events_add_event(nr_analytics_events_t* events,
-                             const nr_log_event_t* event,
-                             nr_random_t* rnd) {
-  bool events_sampled = false;
-  char* log_event_json = NULL;
-  nr_analytics_event_t* log_event = NULL;
-
-  if (NULL == events) {
+bool nr_log_events_is_sampling(nr_log_events_t* events) {
+  if (events->events_used < events->events_allocated) {
     return false;
   }
+  return true;
+}
+
+/*
+ * Safety check for comparator functions
+ *
+ * This avoids NULL checks in each comparator and ensures that NULL
+ * elements are consistently considered as smaller.
+ */
+#define COMPARATOR_NULL_CHECK(__elem1, __elem2)             \
+  if (nrunlikely(NULL == (__elem1) || NULL == (__elem2))) { \
+    if ((__elem1) < (__elem2)) {                            \
+      return -1;                                            \
+    } else if ((__elem1) > (__elem2)) {                     \
+      return 1;                                             \
+    }                                                       \
+    return 0;                                               \
+  }
+
+static int nr_log_event_age_comparator(const nr_log_event_t* a,
+                                       const nr_log_event_t* b) {
+  if (a->timestamp > b->timestamp) {
+    return -1;
+  } else if (a->timestamp < b->timestamp) {
+    return 1;
+  }
+  return 0;
+}
+
+static int nr_log_event_wrapped_age_comparator(const void* a, const void* b) {
+  COMPARATOR_NULL_CHECK(a, b);
+
+  return nr_log_event_age_comparator((const nr_log_event_t*)a,
+                                     (const nr_log_event_t*)b);
+}
+
+static int nr_log_event_priority_comparator(const nr_log_event_t* a,
+                                            const nr_log_event_t* b) {
+  if (a->priority > b->priority) {
+    return 1;
+  } else if (a->priority < b->priority) {
+    return -1;
+  } else {
+    return nr_log_event_wrapped_age_comparator(a, b);
+  }
+}
+
+static int nr_log_event_wrapped_priority_comparator(const void* a,
+                                                    const void* b,
+                                                    void* userdata NRUNUSED) {
+  COMPARATOR_NULL_CHECK(a, b);
+
+  return nr_log_event_priority_comparator((const nr_log_event_t*)a,
+                                          (const nr_log_event_t*)b);
+}
+
+/* All log events popping out of the segment heap go through this
+ * function.
+ *
+ * The log event is discarded (thus removed from the log event heap).
+ */
+static void nr_log_event_discard_wrapper(nr_log_event_t* event,
+                                         void* userdata NRUNUSED) {
+  if (NULL == event) {
+    return;
+  }
+
+  nr_log_event_destroy(&event);
+}
+
+nr_log_events_t* nr_log_events_create(int max_events) {
+  nr_log_events_t* events;
+
+  if (NR_MAX_LOG_EVENTS_MAX_SAMPLES_STORED < max_events) {
+    return NULL;
+  }
+
+  events = (nr_log_events_t*)nr_zalloc(sizeof(nr_log_events_t));
+  events->events_allocated = max_events;
+  events->events_used = 0;
+  events->events_seen = 0;
+
+  if (0 != max_events) {
+    events->events = nr_minmax_heap_create(
+        max_events, nr_log_event_wrapped_priority_comparator, NULL,
+        (nr_minmax_heap_dtor_t)nr_log_event_discard_wrapper, NULL);
+  } else {
+    events->events = NULL;
+  }
+
+  return events;
+}
+
+void nr_log_events_destroy(nr_log_events_t** events_ptr) {
+  nr_log_events_t* events;
+
+  if (NULL == events_ptr) {
+    return;
+  }
+  events = *events_ptr;
+  if (NULL == events) {
+    return;
+  }
+
+  if (NULL != events->events) {
+    nr_minmax_heap_destroy(&events->events);
+  }
+
+  nr_realfree((void**)events_ptr);
+}
+
+int nr_log_events_max_events(const nr_log_events_t* events) {
+  if (NULL == events)
+    return 0;
+
+  return events->events_allocated;
+}
+
+int nr_log_events_number_seen(const nr_log_events_t* events) {
+  if (NULL == events)
+    return 0;
+
+  return events->events_seen;
+}
+
+int nr_log_events_number_saved(const nr_log_events_t* events) {
+  if (NULL == events)
+    return 0;
+
+  return events->events_used;
+}
+
+// const char* nr_log_events_get_event_json(nr_log_events_t* events,
+//                                                   int i) {
+//   return nr_analytics_events_get_event_json(events, i);
+// }
+
+bool nr_log_events_add_event(nr_log_events_t* events,
+                             const nr_log_event_t* event) {
+  bool events_sampled = false;
+  nr_log_event_t* dup_event;
+
   if (NULL == event) {
     return false;
   }
 
-  events_sampled = nr_analytics_events_is_sampling(events);
+  events->events_seen++;
 
-  log_event_json = nr_log_event_to_json(event);
-  log_event = nr_analytics_event_create_from_string(log_event_json);
-  nr_free(log_event_json);
+  // if no event queue exists or size is 0 then event will be dropped
+  if (NULL == events || NULL == events->events || 0 == events->events_allocated) {
+      return true;
+  }
 
-  nr_analytics_events_add_event(events, log_event, rnd);
-  nr_free(log_event);
+  events_sampled = nr_log_events_is_sampling(events);
+
+  // make a copy of the event so the heap can control its entire life cycle
+  dup_event = nr_log_event_clone(event);
+  if (NULL == dup_event)
+    return false;
+
+  nr_minmax_heap_insert(events->events, (void*)dup_event);
+  if (!events_sampled)
+    events->events_used++;
 
   return events_sampled;
 }
--- axiom/nr_log_events.h
+++ axiom/nr_log_events.h
@@ -6,13 +6,14 @@
 #ifndef NR_LOG_EVENTS_HDR
 #define NR_LOG_EVENTS_HDR
 
-#include "nr_analytics_events.h"
+//#include "nr_analytics_events.h"
 #include "nr_log_event.h"
 #include "util_random.h"
 
+typedef struct _nr_log_events_t nr_log_events_t;
+
 /*
- * Purpose : Convert a log event to an analytic event and add it to
- *           an event pool.
+ * Purpose : Add a log event to a log event pool.
  *
  * Returns : true if and only if sampling occured (see Notes below)
  *           false otherwise (sampling not occurred; event was not added
@@ -23,28 +24,15 @@
  *           data structure is full, this event may replace an existing event
  *           based upon a sampling algorithm.
  */
-extern bool nr_log_events_add_event(nr_analytics_events_t* events,
-                                    const nr_log_event_t* event,
-                                    nr_random_t* rnd);
+extern bool nr_log_events_add_event(nr_log_events_t* events,
+                                    const nr_log_event_t* event);
 
-/* Function aliases redirecting to underlying analytics events implementation */
-NRINLINE nr_analytics_events_t* nr_log_events_create(int max_events) {
-  return nr_analytics_events_create_ex(max_events);
-}
-NRINLINE void nr_log_events_destroy(nr_analytics_events_t** events) {
-  return nr_analytics_events_destroy(events);
-}
-NRINLINE int nr_log_events_max_events(const nr_analytics_events_t* events) {
-  return nr_analytics_events_max_events(events);
-}
-NRINLINE int nr_log_events_number_seen(const nr_analytics_events_t* events) {
-  return nr_analytics_events_number_seen(events);
-}
-NRINLINE int nr_log_events_number_saved(const nr_analytics_events_t* events) {
-  return nr_analytics_events_number_saved(events);
-}
-NRINLINE const char* nr_log_events_get_event_json(nr_analytics_events_t* events,
-                                                  int i) {
-  return nr_analytics_events_get_event_json(events, i);
-}
-#endif
+extern nr_log_events_t* nr_log_events_create(int max_events);
+extern void nr_log_events_destroy(nr_log_events_t** events);
+extern int nr_log_events_max_events(const nr_log_events_t* events);
+extern int nr_log_events_number_seen(const nr_log_events_t* events);
+extern int nr_log_events_number_saved(const nr_log_events_t* events);
+extern bool nr_log_events_is_sampling(nr_log_events_t* events);
+// const char* nr_log_events_get_event_json(nr_log_events_t* events, int i);
+
+#endif /* NR_LOG_EVENTS_HDR */
