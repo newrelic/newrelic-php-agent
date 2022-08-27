b5c11b9c5f7b320eb6314a05297744cb87c8beb1
Michal Nowacki <mnowacki@newrelic.com>
2022-08-26 21:26:01 -0400

    tests: add logging integration tests (#502)
    Include Monolog2 and Monolog3 bundles to be used in logging integration
    tests.
    
    Co-authored-by: Michael Fulbright <89205663+mfulb@users.noreply.github.com>

---
+++ tests/integration/logging/monolog3/test_monolog.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Copyright 2022 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Test that Monolog3 instrumentation generates metrics and log events
+*/
+
+/*SKIPIF
+<?php
+
+require('skipif.inc');
+
+*/
+
+/*EXPECT
+monolog3.DEBUG: debug []
+monolog3.INFO: info []
+monolog3.NOTICE: notice []
+monolog3.WARNING: warning []
+monolog3.ERROR: error []
+monolog3.CRITICAL: critical []
+monolog3.ALERT: alert []
+monolog3.EMERGENCY: emergency []
+ */
+
+require_once(realpath(dirname(__FILE__)) . '/../../../include/config.php');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/monolog.php');
+require_monolog(3);
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+
+
+function test_logging() {
+    $logger = new Logger('monolog3');
+
+    $logfmt = "%channel%.%level_name%: %message% %context%\n";
+    $formatter = new LineFormatter($logfmt);
+
+    $stdoutHandler = new StreamHandler('php://stdout', Logger::DEBUG);
+    $stdoutHandler->setFormatter($formatter);
+
+    $logger->pushHandler($stdoutHandler);
+    
+    $logger->debug("debug");
+    $logger->info("info");
+    $logger->notice("notice");
+    $logger->warning("warning");
+    $logger->error("error");
+    $logger->critical("critical");
+    $logger->alert("alert");
+    $logger->emergency("emergency");
+}
+
+test_logging();
