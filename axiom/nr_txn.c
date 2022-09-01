6cac3ddb22b1c70805316d5f624d0692f9b16a82
Brian Duranleau <bduranleau@newrelic.com>
2022-09-01 13:37:12 -0500

    feat(axiom): verify valid log level based on config setting for txns

--- axiom/nr_txn.c
+++ axiom/nr_txn.c
@@ -18,6 +18,7 @@
 #include "nr_header.h"
 #include "nr_limits.h"
 #include "nr_log_events.h"
+#include "nr_log_level.h"
 #include "nr_segment.h"
 #include "nr_segment_private.h"
 #include "nr_segment_traces.h"
@@ -54,8 +55,10 @@ struct _nr_txn_attribute_t {
 #define NR_TXN_ATTRIBUTE_TRACE_ERROR \
   (NR_ATTRIBUTE_DESTINATION_TXN_TRACE | NR_ATTRIBUTE_DESTINATION_ERROR)
 
-#define NR_TXN_ATTR(X, NAME, DESTS) \
-  const nr_txn_attribute_t* X = &(nr_txn_attribute_t) { (NAME), (DESTS) }
+#define NR_TXN_ATTR(X, NAME, DESTS)                     \
+  const nr_txn_attribute_t* X = &(nr_txn_attribute_t) { \
+    (NAME), (DESTS)                                     \
+  }
 
 NR_TXN_ATTR(nr_txn_request_uri,
             "request.uri",
@@ -3267,6 +3270,37 @@ bool nr_txn_log_forwarding_enabled(nrtxn_t* txn) {
   return true;
 }
 
+bool nr_txn_log_forwarding_log_level_verify(nrtxn_t* txn,
+                                            const char* log_level_name) {
+  int log_level;
+
+  if (NULL == txn) { /* more like an assert */
+    return false;
+  }
+
+  if (!txn->options.logging_enabled || !txn->options.log_forwarding_enabled) {
+    return false;
+  }
+
+  if (txn->high_security) {
+    return false;
+  }
+
+  log_level = nr_log_level_str_to_int(log_level_name);
+
+  // pass through UNKNOWN by default
+  if (LOG_LEVEL_UNKNOWN == log_level) {
+    return true;
+  }
+
+  // log levels are organized 0 -> 7 in decreasing severity
+  if (log_level > txn->options.log_forwarding_log_level) {
+    return false;
+  }
+
+  return true;
+}
+
 bool nr_txn_log_metrics_enabled(nrtxn_t* txn) {
   if (NULL == txn) { /* more like an assert */
     return false;
@@ -3359,6 +3393,10 @@ static void nr_txn_add_log_event(nrtxn_t* txn,
   }
 
   if (nr_strempty(log_message)) {
+    return;
+  }
+
+  if (!nr_txn_log_forwarding_log_level_verify(txn, log_level_name)) {
     return;
   }
 
