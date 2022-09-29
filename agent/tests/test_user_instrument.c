83ec2c5253ca0948f136187e75228da8dbc3941b
Amber Sistla <asistla@newrelic.com>
2022-09-28 23:31:59 -0700

    feat(agent): Update test cases for nr_php_get_wraprec_by_name.

--- agent/tests/test_user_instrument.c
+++ agent/tests/test_user_instrument.c
@@ -14,19 +14,67 @@ tlib_parallel_info_t parallel_info
 
 static void test_op_array_wraprec(TSRMLS_D) {
   zend_op_array oparray = {.function_name = (void*)1};
+#if ZEND_MODULE_API_NO < ZEND_8_0_X_API_NO \
+    || defined OVERWRITE_ZEND_EXECUTE_DATA
   nruserfn_t func = {0};
+#else
+  zend_function zend_func = {0};
+  zend_string* scope_name = NULL;
+  zend_class_entry ce = {0};
+  nruserfn_t * wraprec = NULL;
+  char * name_str = "my_func_name";
+#endif
 
   tlib_php_request_start();
 
-  nr_php_op_array_set_wraprec(&oparray, &func TSRMLS_CC);
+
 #if ZEND_MODULE_API_NO < ZEND_8_0_X_API_NO \
     || defined OVERWRITE_ZEND_EXECUTE_DATA
+  nruserfn_t func = {0};
+  nr_php_op_array_set_wraprec(&oparray, &func TSRMLS_CC);
+
   tlib_pass_if_ptr_equal("obtain instrumented function",
                          nr_php_op_array_get_wraprec(&oparray TSRMLS_CC),
                          &func);
 #else
+  wraprec = nr_php_add_custom_tracer_named(name_str, nr_strlen(name_str));
+  /*
+   * NULL if there's no match.
+   */
   tlib_pass_if_ptr_equal("obtain instrumented function",
-                         nr_php_get_wraprec_by_name(func), &func);
+                         nr_php_get_wraprec_by_name(&zend_func), NULL);
+
+  zend_func.common.function_name = zend_string_init(name_str,
+                                                    strlen(name_str), 0);
+  /*
+   * NULL if name is correct but type is wrong.
+   */
+  tlib_pass_if_ptr_equal("obtain instrumented function",
+                         nr_php_get_wraprec_by_name(&zend_func), NULL);
+  /*
+   * Valid if function name and type match.
+   */
+  zend_func.type = ZEND_USER_FUNCTION;
+  tlib_pass_if_ptr_equal("obtain instrumented function",
+                         nr_php_get_wraprec_by_name(&zend_func), wraprec);
+  /*
+   * Null if function name matches, but class name doesn't.
+   */
+  scope_name = zend_string_init("ClassName",
+                                strlen("ClassName"), 0);
+  ce.name = scope_name;
+  zend_func.common.scope = &ce;
+  tlib_pass_if_ptr_equal("obtain instrumented function",
+                         nr_php_get_wraprec_by_name(&zend_func), NULL);
+  /*
+   * Valid if function name matches and class name matches.
+   */
+  scope_name = zend_string_init("ClassName",
+                                strlen("ClassName"), 0);
+
+  wraprec = nr_php_add_custom_tracer_named("ClassName::my_func_name", nr_strlen("ClassName::my_func_name"));
+  tlib_pass_if_ptr_equal("obtain instrumented function",
+                         nr_php_get_wraprec_by_name(&zend_func), wraprec);
 #endif
 
 #if ZEND_MODULE_API_NO >= ZEND_7_3_X_API_NO
@@ -43,7 +91,9 @@ static void test_op_array_wraprec(TSRMLS_D) {
    * Not NULL because we don't care about the reserved array anymore.
    */
   tlib_pass_if_ptr_equal("obtain instrumented function",
-                         nr_php_get_wraprec_by_name(func), &func);
+                         nr_php_get_wraprec_by_name(&zend_func), wraprec);
+
+
 #endif
       /*
        * Restore the cached pid and invalidate the mangled pid/index value.
@@ -67,7 +117,9 @@ static void test_op_array_wraprec(TSRMLS_D) {
    * Not NULL because we don't care about the reserved array anymore.
    */
   tlib_pass_if_ptr_equal("obtain instrumented function",
-                         nr_php_get_wraprec_by_name(func), &func);
+                         nr_php_get_wraprec_by_name(&zend_func), wraprec);
+  zend_string_release(zend_func.common.function_name);
+  zend_string_release(zend_func.common.scope->name);
 #endif
 #endif /* PHP >= 7.3 */
 
