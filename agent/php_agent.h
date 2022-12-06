612bb23d0544f7e8dff1b7b4928955ed1c950071
Amber Sistla <ambersistla@gmail.com>
2022-12-06 09:58:00 -0700

    Update php_agent.h

--- agent/php_agent.h
+++ agent/php_agent.h
@@ -794,12 +794,11 @@ extern bool nr_php_function_is_static_method(const zend_function* func);
 extern zend_execute_data* nr_get_zend_execute_data(NR_EXECUTE_PROTO TSRMLS_DC);
 
 /*
- * Purpose : If code level metrics are enabled, extract the data from the OAPI
- *           given zend_execute_data.  Add the CLM as agent attributes to the
- *           attributes data structure.
+ * Purpose : If code level metrics are enabled, extract the data segment metadata.  
+ *           Add the CLM as agent attributes to the attributes data structure.
  *
  * Params  : 1. attributes data structure to add the CLM to
- *           2. The zend_execute_data given by OAPI
+ *           2. metadata that contains the information about the segment
  *
  * Returns : void
  *
