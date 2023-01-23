10bd00803cec4ec7f2a9ca2cc5707c533583d40c
Amber Sistla <ambersistla@gmail.com>
2023-01-23 08:22:48 -0700

    fix(tests): Account for PHP 8.1 deprecation of implicit int to float conversion

---
+++ tests/integration/jit/tracing/test_computations.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Transaction event generated for heavy computation.  No errors or failures generated despite creating spans for a huge number of calls.
+*/
+
+/*SKIPIF
+<?php
+
+require('skipif.inc');
+
+*/
+
+/*INI
+newrelic.distributed_tracing_enabled=1
+newrelic.transaction_tracer.threshold = 0
+error_reporting = E_ALL
+opcache.enable=1
+opcache.enable_cli=1
+opcache.file_update_protection=0
+opcache.jit_buffer_size=32M
+opcache.jit=function
+*/
+
+/*EXPECT_ANALYTICS_EVENTS
+[
+  "?? agent run id",
+  {
+    "reservoir_size": 50,
+    "events_seen": 1
+  },
+  [
+    [
+      {
+        "traceId": "??",
+        "duration": "??",
+        "timestamp": "??",
+        "type": "Transaction",
+        "name": "OtherTransaction\/php__FILE__",
+        "guid": "??",
+        "priority": "??",
+        "sampled": true,
+        "totalTime": "??",
+        "error": false
+      },
+      {},
+      {}
+    ]
+  ]
+]
+*/
+
+/*EXPECT
+Hello
+*/
+
+newrelic_custom_tracer(computation);
+
+function computation(float $a): int
+{
+
+    $b = intval($a) % (2 ** 32);
+    return $b;
+}
+
+for ($i = 0; $i < 500; ++$i) {
+    computation(2**64);
+}
+echo 'Hello';
