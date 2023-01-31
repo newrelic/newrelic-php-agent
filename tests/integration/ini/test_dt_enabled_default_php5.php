5dcd2c8a1ccc78581fb1699ebf82d5095fe034ee
Brian Duranleau <bduranleau@newrelic.com>
2023-01-31 13:07:06 -0600

    tests(clm): generate duplicate tests w/CLM disabled for PHP 5

---
+++ tests/integration/ini/test_dt_enabled_default_php5.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Distributed tracing should be enabled by default.  Verify by seeing 
+that span events are sent when span events are enabled and no other 
+configuration is given.
+*/
+
+/*INI
+newrelic.transaction_tracer.threshold = 0
+newrelic.span_events_enabled = 1
+newrelic.code_level_metrics.enabled=false
+*/
+
+/*EXPECT_SPAN_EVENTS
+[
+  "?? agent run id",
+  {
+    "reservoir_size": 10000,
+    "events_seen": 2
+  },
+  [
+    [
+      {
+        "traceId": "??",
+        "duration": "??", 
+        "transactionId": "??",
+        "name": "OtherTransaction\/php__FILE__",
+        "guid": "??",
+        "type": "Span",
+        "category": "generic",
+        "priority": "??", 
+        "sampled": true,
+        "nr.entryPoint": true,
+        "timestamp": "??",
+        "transaction.name": "OtherTransaction\/php__FILE__"
+      },
+      {},
+      {}
+    ],
+    [
+      {
+        "traceId": "??",
+        "duration": "??",
+        "transactionId": "??",
+        "name": "Custom\/main",
+        "guid": "??",
+        "type": "Span",
+        "category": "generic",
+        "priority": "??",
+        "parentId": "??",
+        "sampled": true,
+        "timestamp": "??"
+      },
+      {},
+      {}
+    ]
+  ]
+]
+*/
+
+/*EXPECT
+Hello
+*/
+
+newrelic_add_custom_tracer('main');
+function main()
+{
+  echo 'Hello';
+}
+main();
+
