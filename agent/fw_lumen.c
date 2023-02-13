63e889962206aea00c268c65faacf511a301eddd
Amber Sistla <asistla@newrelic.com>
2023-02-13 15:40:58 -0700

    fix(agent): Lumen exception needs txn naming before.

--- agent/fw_lumen.c
+++ agent/fw_lumen.c
@@ -148,11 +148,12 @@ NR_PHP_WRAPPER_END
  * Exception handling logic. Wraps the function that routes
  * exceptions to their respective handlers
  *
- * In this case, `nr_txn_set_path` is called after `NR_PHP_WRAPPER_CALL` with
+ * txn naming scheme:
+ * In this case, `nr_txn_set_path` is called before `NR_PHP_WRAPPER_CALL` with
  * `NR_OK_TO_OVERWRITE` and as this corresponds to calling the wrapped function
- * in func_end no change is needed to ensure OAPI compatibility as it will use
- * the default func_end after callback. This entails that the first wrapped
- * function call of this type gets to name the txn.
+ * in func_begin it needs to be explicitly set as a before_callback to ensure
+ * OAPI compatibility. This entails that the last wrapped call gets to name the
+ * txn which in this case is the one that generated the exception.
  */
 NR_PHP_WRAPPER(nr_lumen_exception) {
   zval* exception = NULL;
@@ -218,8 +219,14 @@ void nr_lumen_enable(TSRMLS_D) {
   nr_php_wrap_user_function(
       NR_PSTR("Laravel\\Lumen\\Application::handleFoundRoute"),
       nr_lumen_handle_found_route TSRMLS_CC);
-
+#if ZEND_MODULE_API_NO >= ZEND_8_0_X_API_NO \
+    && !defined OVERWRITE_ZEND_EXECUTE_DATA
+  nr_php_wrap_user_function_before_after_clean(
+      NR_PSTR("Laravel\\Lumen\\Application::sendExceptionToHandler"),
+      nr_lumen_exception, NULL, NULL);
+#else
   nr_php_wrap_user_function(
       NR_PSTR("Laravel\\Lumen\\Application::sendExceptionToHandler"),
       nr_lumen_exception TSRMLS_CC);
+#endif
 }
