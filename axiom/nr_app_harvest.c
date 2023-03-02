16b9022b2eb62739b5439e3e81eeae3a92032b5b
bduranleau-nr <106178551+bduranleau-nr@users.noreply.github.com>
2023-03-02 15:40:23 -0600

    fix: Remove ARM64 glibc pin

--- axiom/nr_app_harvest.c
+++ axiom/nr_app_harvest.c
@@ -25,8 +25,6 @@
 #if ((__GLIBC__) >= 2) && ((__GLIBC_MINOR__) > 17)
 #if defined (__x86_64__)
   __asm__(".symver pow,pow@GLIBC_2.2.5");
-#elif defined (__aarch64__)
-  __asm__(".symver pow,pow@GLIBC_2.17");
 #endif
 #endif
 #endif
