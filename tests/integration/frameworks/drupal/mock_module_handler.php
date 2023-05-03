4b2e3aa8e2851dd79c4f59efada370feff931f37
Zach Neumann <zneumann@newrelic.com>
2023-05-03 14:25:26 -0600

    test: test drupal 9.4+ hooks instrumentation

---
+++ tests/integration/frameworks/drupal/mock_module_handler.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This modifies interfaces from the Symfony project.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+/* Mock enough bits of the Module Handler Interface for hook tests. */
+
+namespace Drupal\Core\Extension {
+    interface ModuleHandlerInterface {
+        public function invokeAllWith($hook_str, $callback);
+    }
+    class ModuleHandler implements ModuleHandlerInterface {
+        public function invokeAllWith($hook_str, $callback) {
+            if ($hook_str == "hook_1") {
+                $module = "module_a";
+                $callback($module . "_" . $hook_str, $module);
+                $module = "module_b";
+                $callback($module . "_" . $hook_str, $module);
+            } else if ($hook_str == "hook_2") {
+                $module = "module_b";
+                $callback($module . "_" . $hook_str, $module);
+            } else if ($hook_str == "hook_3") {
+                $module = "module_a";
+                $callback($module . "_" . $hook_str, $module);
+            }
+        }
+    }
+}
