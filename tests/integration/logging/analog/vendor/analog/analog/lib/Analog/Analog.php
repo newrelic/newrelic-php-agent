14b3747ab9bac7192dc60d463f00e8bcc1d6a2d8
Michal Nowacki <mnowacki@newrelic.com>
2023-07-25 14:05:55 -0400

    enhance integration tests logging libraries mocks
    Empty files don't get executed by PHP 8.2 therefore mocks needs to do something.

--- tests/integration/logging/analog/vendor/analog/analog/lib/Analog/Analog.php
+++ tests/integration/logging/analog/vendor/analog/analog/lib/Analog/Analog.php
@@ -1,3 +1,6 @@
 <?php
 
 /* This is a mock of Analog library as it would have been installed by a composer */
+
+/* Empty files don't get executed by PHP 8.2 therefore the mock needs to do something */
+echo "";
--- tests/integration/logging/consolidation-log/vendor/consolidation/log/src/Logger.php
+++ tests/integration/logging/consolidation-log/vendor/consolidation/log/src/Logger.php
@@ -1,3 +1,6 @@
 <?php
 
 /* This is a mock of consolidation-log library as it would have been installed by a composer */
+
+/* Empty files don't get executed by PHP 8.2 therefore the mock needs to do something */
+echo "";
