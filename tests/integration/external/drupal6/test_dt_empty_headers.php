aaf84e2d142afd08456dbc4b3edeeb58a090cb7c
Michal Nowacki <mnowacki@newrelic.com>
2023-07-26 10:57:36 -0400

    tests(agent): add Drupal integration tests (#696)

---
+++ tests/integration/external/drupal6/test_dt_empty_headers.php
@@ -0,0 +1,63 @@
+<?php
+
+/*DESCRIPTION
+Test that DT works with drupal_http_request when called with an empty header
+array.
+*/
+
+// force the framework to avoid requiring the drupal detection file
+/*INI
+newrelic.framework = drupal
+newrelic.distributed_tracing_enabled = true
+newrelic.cross_application_tracer.enabled = false
+*/
+
+/*SKIPIF
+<?php
+require("skipif.inc");
+*/
+
+/*EXPECT
+traceparent=found tracestate=found newrelic=found X-NewRelic-ID=missing X-NewRelic-Transaction=missing tracing endpoint reached
+*/
+
+/*EXPECT_RESPONSE_HEADERS
+*/
+
+/*EXPECT_METRICS
+[
+  "?? agent run id",
+  "?? timeframe start",
+  "?? timeframe stop",
+  [
+    [{"name": "Supportability/Logging/Forwarding/PHP/enabled"},    [1, "??", "??", "??", "??", "??"]],
+    [{"name": "Supportability/Logging/Metrics/PHP/enabled"},       [1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/127.0.0.1/all"},                   [1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/127.0.0.1/all", 
+      "scope":"OtherTransaction/php__FILE__"},            [1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/all"},                             [1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/allOther"},                        [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransaction/all"},                     [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransaction/php__FILE__"},             [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransactionTotalTime"},                [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransactionTotalTime/php__FILE__"},    [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/framework/Drupal/forced"},   [1,    0,    0,    0,    0,    0]],
+    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/all"}, 
+                                                          [1, "??", "??", "??", "??", "??"]],
+    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/allOther"}, 
+                                                          [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/TraceContext/Create/Success"},    
+							  [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/DistributedTrace/CreatePayload/Success"}, 
+                                                          [1, "??", "??", "??", "??", "??"]]
+  ]
+]
+*/
+
+require_once(realpath(dirname(__FILE__)) . '/../../../include/drupal_6_bootstrap.inc');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/drupal_6_common.inc');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/config.php');
+
+$url = "http://" . make_tracing_url(realpath(dirname(__FILE__)) . '/../../../include/tracing_endpoint.php');
+$rv = drupal_http_request($url, array());
+echo $rv->data;
---
+++ tests/integration/external/drupal7/test_synthetics_with_dt.php
@@ -0,0 +1,80 @@
+<?php
+
+/*DESCRIPTION
+The agent SHALL add X-NewRelic-Synthetics headers to external calls when
+the current request is a synthetics request and DT is used.
+*/
+
+// force the framework to avoid requiring the drupal detection file
+/*INI
+newrelic.framework = drupal
+newrelic.distributed_tracing_enabled = true
+*/
+
+/*SKIPIF
+<?php
+require("skipif.inc");
+*/
+
+/*
+ * The synthetics header contains the following JSON.
+ *   [
+ *     1,
+ *     432507,
+ *     "rrrrrrr-rrrr-1234-rrrr-rrrrrrrrrrrr",
+ *     "jjjjjjj-jjjj-1234-jjjj-jjjjjjjjjjjj",
+ *     "mmmmmmm-mmmm-1234-mmmm-mmmmmmmmmmmm"
+ *   ]
+ */
+
+/*HEADERS
+X-NewRelic-Synthetics=PwcbVVVRDQMHSEMQRUNFFBZDG0EQFBFPAVALVhVKRkBBSEsTQxNBEBZERRMUERofEg4LCF1bXQxJW1xZCEtSUANWFQhSUl4fWQ9TC1sLWQgOXF0LRE8aXl0JDA9aXBoLCVxbHlNUUFYdD1UPVRVZX14IVAxcDF4PCVsVPA==
+*/
+
+/*EXPECT
+traceparent=found tracestate=found newrelic=found X-NewRelic-ID=missing X-NewRelic-Transaction=missing X-NewRelic-Synthetics=found tracing endpoint reached
+*/
+
+/*EXPECT_RESPONSE_HEADERS
+*/
+
+/*EXPECT_METRICS
+[
+  "?? agent run id",
+  "?? start time",
+  "?? stop time",
+  [
+    [{"name": "Supportability/Logging/Forwarding/PHP/enabled"},    [1, "??", "??", "??", "??", "??"]],
+    [{"name": "Supportability/Logging/Metrics/PHP/enabled"},       [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Apdex"},                                    ["??", "??", "??", "??", "??",    0]],
+    [{"name":"Apdex/Uri__FILE__"},                        ["??", "??", "??", "??", "??",    0]],
+    [{"name":"External/all"},                             [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/allWeb"},                          [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/127.0.0.1/all"},                   [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"External/127.0.0.1/all", 
+      "scope":"WebTransaction/Uri__FILE__"},              [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"HttpDispatcher"},                           [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/framework/Drupal/forced"},   [   1,    0,    0,    0,    0,    0]],
+    [{"name":"WebTransaction"},                           [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"WebTransaction/Uri__FILE__"},               [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"WebTransactionTotalTime"},                  [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"WebTransactionTotalTime/Uri__FILE__"},      [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/all"}, 
+                                                          [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/allWeb"}, 
+                                                          [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/TraceContext/Create/Success"},    
+							  [   1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/DistributedTrace/CreatePayload/Success"}, 
+                                                          [   1, "??", "??", "??", "??", "??"]]
+  ]
+]
+*/
+
+require_once(realpath(dirname(__FILE__)) . '/../../../include/drupal_7_bootstrap.inc');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/drupal_7_common.inc');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/config.php');
+
+$url = "http://" . make_tracing_url(realpath(dirname(__FILE__)) . '/../../../include/tracing_endpoint.php');
+$rv = drupal_http_request($url);
+echo $rv->data;
