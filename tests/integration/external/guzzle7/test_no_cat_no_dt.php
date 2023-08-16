3cec1fedf8bd827ea6279122145822d1c69d700b
Michal Nowacki <mnowacki@newrelic.com>
2023-08-16 14:02:46 -0400

    add tests for version 7 of guzzle
    Version 6 of Guzzle supports PHPs up to but not including 8.0. In order to test
    agent's instrumentation of guzzle on PHPs 8.0+, version 7 of Guzzle needs to
    be used. There are no fundamental changes in version 7 of Guzzle therefore
    agent's instrumentation of version 6 of guzzle6 works for version 7 too. However,
    this results in guzzle7 being detected as guzzle6 and that's why metrics in
    integration tests expectation include `Supportability/library/Guzzle 6/detected`
    as well as `Supportability/library/Guzzle 4-5/detected`.
    
    guzzle.tar.bz2 bundle has been updated to include version 7 of Guzzle.

---
+++ tests/integration/external/guzzle7/test_no_cat_no_dt.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Test that Cross Application Tracing (CAT) works with guzzle 7.
+*/
+
+/*SKIPIF
+<?php
+require("skipif.inc");
+*/
+
+/*INI
+newrelic.cross_application_tracer.enabled = false
+newrelic.distributed_tracing_enabled=0
+*/
+
+/*EXPECT
+X-NewRelic-ID=missing X-NewRelic-Transaction=missing tracing endpoint reached
+X-NewRelic-ID=missing X-NewRelic-Transaction=missing tracing endpoint reached
+X-NewRelic-ID=missing X-NewRelic-Transaction=missing Customer-Header=found tracing endpoint reached
+*/
+
+/*EXPECT_RESPONSE_HEADERS
+*/
+
+/*EXPECT_METRICS
+[
+  "?? agent run id",
+  "?? timeframe start",
+  "?? timeframe stop",
+  [
+    [{"name":"External/127.0.0.1/all"},                   [3, "??", "??", "??", "??", "??"]],
+    [{"name":"External/127.0.0.1/all", 
+      "scope":"OtherTransaction/php__FILE__"},            [3, "??", "??", "??", "??", "??"]],
+    [{"name":"External/all"},                             [3, "??", "??", "??", "??", "??"]],
+    [{"name":"External/allOther"},                        [3, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransaction/all"},                     [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransaction/php__FILE__"},             [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransactionTotalTime"},                [1, "??", "??", "??", "??", "??"]],
+    [{"name":"OtherTransactionTotalTime/php__FILE__"},    [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/library/Guzzle 4-5/detected"},         [2, 0, 0, 0, 0, 0]],
+    [{"name":"Supportability/library/Guzzle 6/detected"},           [1, 0, 0, 0, 0, 0]],
+    [{"name":"Supportability/Logging/Forwarding/PHP/enabled"},      [1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/Logging/Metrics/PHP/enabled"},         [1, "??", "??", "??", "??", "??"]]
+  ]
+]
+*/
+
+?>
+<?php
+require_once(realpath(dirname(__FILE__)) . '/../../../include/config.php');
+require_once(realpath(dirname(__FILE__)) . '/../../../include/unpack_guzzle.php');
+require_guzzle(7);
+
+/* Create URL. */
+$url = "http://" . make_tracing_url(realpath(dirname(__FILE__)) .  '/../../../include/tracing_endpoint.php');
+
+/* Use guzzle 7 to make an http request. */
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->get($url);
+echo $response->getBody();
+
+$response = $client->get($url, [
+    'headers' => [
+        'zip' => 'zap']]);
+echo $response->getBody();
+
+$response = $client->get($url, [
+    'headers' => [
+        'zip' => 'zap',
+        CUSTOMER_HEADER => 'zap']]);
+echo $response->getBody();
