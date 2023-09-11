0a25ba7ef08632c8f950a3b5ca88029e176cc109
bduranleau-nr <106178551+bduranleau-nr@users.noreply.github.com>
2023-09-11 12:41:28 -0500

    feat: add newrelic_set_user_id API (#723)
    Implement `newrelic_set_user_id` API functionality in the PHP Agent.
    
    Allows an application to track a user via calling a new
    `newrelic_set_user_id` API, providing a UUID string as an argument.
    
    If the API call is successful, an agent attribute `enduser.id` will be
    generated containing the provided UUID string.

--- agent/php_api.c
+++ agent/php_api.c
@@ -1580,6 +1580,71 @@ PHP_FUNCTION(newrelic_get_trace_metadata) {
 }
 
 /*
+ * Purpose      Allows a caller to add a user id string to agent attributes in 
+                transaction event, transaction trace, error and span.
+ *
+ */
+#ifdef TAGS
+void zif_newrelic_set_user_id(void); /* ctags landing pad only */
+void newrelic_set_user_id(void);     /* ctags landing pad only */
+#endif
+PHP_FUNCTION(newrelic_set_user_id) {
+  zval* uuid_zv = NULL;
+  char* uuid_str = NULL;
+
+  NR_UNUSED_RETURN_VALUE_PTR;
+  NR_UNUSED_THIS_PTR;
+  NR_UNUSED_RETURN_VALUE_USED;
+
+  if (!nr_php_recording(TSRMLS_C)) {
+    RETURN_FALSE;
+  }
+
+  nr_php_api_add_supportability_metric("set_user_id" TSRMLS_CC);
+
+  if (nrunlikely(NULL == NRPRG(txn))) {
+    nrl_verbosedebug(NRL_API, "newrelic_set_user_id failure: txn is NULL.");
+    RETURN_FALSE;
+  }
+
+  if (1 != ZEND_NUM_ARGS()) {
+    nrl_warning(NRL_API,
+                "newrelic_set_user_id failure: invalid number of parameters.");
+    RETURN_FALSE;
+  }
+
+  if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS(), "z", &uuid_zv)) {
+    nrl_warning(NRL_API,
+                "newrelic_set_user_id failure: invalid argument passed.");
+    RETURN_FALSE;
+  }
+
+  if (!nr_php_is_zval_non_empty_string(uuid_zv)) {
+    nrl_warning(
+        NRL_API,
+        "newrelic_set_user_id failure: User ID must be a non-empty string.");
+    RETURN_FALSE;
+  }
+
+  if (NR_ATTRIBUTE_VALUE_LENGTH_LIMIT < Z_STRLEN_P(uuid_zv)) {
+    nrl_warning(NRL_API,
+                "newrelic_set_user_id_failure: invalid string length.");
+    RETURN_FALSE;
+  }
+
+  uuid_str = nr_strndup(Z_STRVAL_P(uuid_zv), Z_STRLEN_P(uuid_zv));
+
+  nr_attributes_agent_add_string(
+      NRPRG(txn)->attributes,
+      (NR_ATTRIBUTE_DESTINATION_TXN_EVENT | NR_ATTRIBUTE_DESTINATION_TXN_TRACE
+       | NR_ATTRIBUTE_DESTINATION_ERROR | NR_ATTRIBUTE_DESTINATION_SPAN),
+      "enduser.id", uuid_str);
+
+  nr_free(uuid_str);
+  RETURN_TRUE;
+}
+
+/*
  * Purpose      (New Relic API) (Error Fingerprinting)
  *              Allows a user to register a PHP callback to be invoked by the
  *              Agent when the application encounters an error for the purpose
