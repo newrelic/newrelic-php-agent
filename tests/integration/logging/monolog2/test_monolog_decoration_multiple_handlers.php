9840a6712ba2a0f4ecc12f358132cd6854e09bc0
Michael Fulbright <mfulbright@newrelic.com>
2023-09-26 13:33:01 -0400

    chore(tests): Refactors to reduce dupe code

--- tests/integration/logging/monolog2/test_monolog_decoration_multiple_handlers.php
+++ tests/integration/logging/monolog2/test_monolog_decoration_multiple_handlers.php
@@ -184,44 +184,7 @@ use Monolog\Handler\ProcessHandler;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-/* create formatter class that echos the interesting data that the log 
-   decoration should have added. */
-class CheckDecorateFormatter implements Monolog\Formatter\FormatterInterface {
-  public function __construct(?string $dateFormat = null) {
-  }
-  public function format(array $record) {
-    $nrlinking = $record['extra']['NR-LINKING'] ?? 'NR-LINKING DATA MISSING!!!';
-    $result = preg_match("/(NR\-LINKING)\|([\w\d]+)\|([\w\d]+)\|([\w\d]+)\|([\w\d]+)\|([\w\d\%]+\.php)\|/", $nrlinking, $matches);
-    $linkmeta = newrelic_get_linking_metadata();
-
-    tap_equal(7, count($matches), "All NR-LINKING elements present");
-    if (7 == count($matches)) {
-      tap_equal("NR-LINKING", $matches[1], "NR-LINKING present");
-      tap_equal($linkmeta['entity.guid'] ?? '<missing entity.guid>', $matches[2], "entity.guid correct");
-      tap_equal($linkmeta['hostname'] ?? '<missing hostname>', $matches[3], "entity.guid correct");
-      tap_equal($linkmeta['trace.id'] ?? '<missing trace.id>', $matches[4], "trace.id correct");
-      tap_equal(true, strlen($matches[5]) > 0 && preg_match("/[\w\d]+/",$matches[5]), "span.id is non-zero length and alphanumeric");
-      if (isset($linkmeta['entity.name'])) {
-        $name = urlencode($linkmeta['entity.name']);
-      } else {
-        $name = '<missing entity.name>';
-      }
-      tap_equal($name, $matches[6], "entity.name correct");
-    }
-
-    /* have to return a non-null value which is the output string */
-    return "";
-  }
-
-  public function formatBatch(array $records) {
-    foreach ($records as $key => $record) {
-      $records[$key] = $this->format($record);
-    }
-
-    return $records;  
-  }
-}
-
+require_once('checkdecorateformatter.php');
 
 function test_logging() {
     $logger = new Logger('monolog2');
@@ -238,7 +201,6 @@ function test_logging() {
     $logger->pushHandler($noopHandler);
     $logger->pushHandler($processHandler);
 
-  
     // insert delays between log messages to allow priority sampling
     // to resolve that later messages have higher precedence
     // since timestamps are only millisecond resolution
