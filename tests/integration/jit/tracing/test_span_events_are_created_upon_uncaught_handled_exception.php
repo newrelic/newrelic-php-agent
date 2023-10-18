6a0e68682efa14b34ec88494c68efb71d941c09d
Amber Sistla <asistla@newrelic.com>
2023-10-18 15:41:22 -0700

    Update tests/integration/jit/tracing/test_span_events_are_created_upon_uncaught_handled_exception.php
    Co-authored-by: Michal Nowacki <mnowacki@newrelic.com>

--- tests/integration/jit/tracing/test_span_events_are_created_upon_uncaught_handled_exception.php
+++ tests/integration/jit/tracing/test_span_events_are_created_upon_uncaught_handled_exception.php
@@ -30,8 +30,6 @@ opcache.enable_cli=1
 opcache.file_update_protection=0
 opcache.jit_buffer_size=32M
 opcache.jit=tracing
-newrelic.loglevel = verbosedebug
-newrelic.daemon.loglevel = debug
 */
 
 /*PHPMODULES
