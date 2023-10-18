5ec542c1de121ed14eabc24a1a5abe8b7490f84f
Michael Fulbright <mfulbright@newrelic.com>
2023-10-18 15:54:53 -0400

    chore: Fixes formatting

--- axiom/tests/test_log_event.c
+++ axiom/tests/test_log_event.c
@@ -377,7 +377,7 @@ static void test_log_event_context_attributes(void) {
   // Test : Get context attributes with a NULL event
   tlib_pass_if_null("Initialize event should have NULL context",
                     event->context_attributes);
-  
+
   // Test: Setting context data on NULL event ptr should not crash
   attributes = nr_attributes_create(NULL);
   nr_log_event_set_context_attributes(NULL, attributes);
@@ -387,7 +387,6 @@ static void test_log_event_context_attributes(void) {
   nr_log_event_set_context_attributes(event, NULL);
 
   nr_log_event_destroy(&event);
-
 }
 
 tlib_parallel_info_t parallel_info = {.suggested_nthreads = 1, .state_size = 0};
