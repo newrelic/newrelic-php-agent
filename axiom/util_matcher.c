d5c08459ef3b6bbffc69220fa4f231555e33a8d3
Brian Duranleau <bduranleau@newrelic.com>
2023-11-13 19:16:52 -0600

    feat(agent,axiom): initial add of matcher logic

---
+++ axiom/util_matcher.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "util_matcher.h"
+#include "util_matcher_private.h"
+
+#include "util_memory.h"
+#include "util_strings.h"
+
+static void nr_matcher_prefix_dtor(void* prefix, void* userdata NRUNUSED) {
+  nr_free(prefix);
+}
+
+nr_matcher_t* nr_matcher_create(void) {
+  nr_matcher_t* matcher;
+
+  matcher = nr_malloc(sizeof(nr_matcher_t));
+  nr_vector_init(&matcher->prefixes, 0, nr_matcher_prefix_dtor, NULL);
+
+  return matcher;
+}
+
+void nr_matcher_destroy(nr_matcher_t** matcher_ptr) {
+  if (NULL == matcher_ptr || NULL == *matcher_ptr) {
+    return;
+  }
+
+  nr_vector_deinit(&((*matcher_ptr)->prefixes));
+  nr_realfree((void**)matcher_ptr);
+}
+
+bool nr_matcher_add_prefix(nr_matcher_t* matcher, const char* prefix) {
+  size_t i;
+  char* prefix_lc;
+  size_t prefix_len;
+
+  if (NULL == matcher || NULL == prefix) {
+    return false;
+  }
+
+  prefix_len = nr_strlen(prefix);
+  while (prefix_len > 0 && '/' == prefix[prefix_len - 1]) {
+    prefix_len--;
+  }
+
+  prefix_lc = nr_malloc(prefix_len + 2);
+  for (i = 0; i < prefix_len; i++) {
+    prefix_lc[i] = nr_tolower(prefix[i]);
+  }
+  prefix_lc[prefix_len] = '/';
+  prefix_lc[prefix_len + 1] = '\0';
+
+  return nr_vector_push_back(&matcher->prefixes, prefix_lc);
+}
+
+char* nr_matcher_match(nr_matcher_t* matcher, const char* input) {
+  size_t i;
+  char* input_lc;
+  char* match = NULL;
+  size_t num_prefixes;
+
+  if (NULL == matcher || NULL == input) {
+    return NULL;
+  }
+
+  num_prefixes = nr_vector_size(&matcher->prefixes);
+  input_lc = nr_string_to_lowercase(input);
+
+  for (i = 0; i < num_prefixes; i++) {
+    const char* found;
+    const char* prefix = nr_vector_get(&matcher->prefixes, i);
+
+    found = nr_strstr(input, prefix);
+    if (found) {
+      const char* slash;
+
+      found += nr_strlen(prefix);
+      slash = nr_strchr(found, '/');
+      if (NULL == slash) {
+        match = nr_strdup(found);
+      } else {
+        match = nr_strndup(found, slash - found);
+      }
+      break;
+    }
+  }
+
+  nr_free(input_lc);
+  return match;
+}
