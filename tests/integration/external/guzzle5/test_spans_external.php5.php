78c55dcfffa0e74f927a371a835baa8ba288afef
Michal Nowacki <mnowacki@newrelic.com>
2023-11-15 16:42:22 -0500

    tests: fix guzzle integration tests (#772)
    Guzzle integration tests are flaky - they expect a span for a call to 
    `new GuzzleHttp\Client();`.  However, when `transaction_tracer.detail`
    is set to 0, the agent sometimes drops it.  On the other hand when
    `transaction_tracer.detail` is set to 1, the agent generates many more 
    spans and adding them to `EXPECT_SPAN_EVENTS` seems tedious. Therefore
    `EXPECT_SPAN_EVENTS_LIKE` is used in test expectations to check if the 
    required spans - root and external - are present.

--- tests/integration/external/guzzle5/test_spans_external.php5.php
+++ tests/integration/external/guzzle5/test_spans_external.php5.php
@@ -23,14 +23,8 @@ newrelic.code_level_metrics.enabled=false
 /*EXPECT_RESPONSE_HEADERS
 */
 
-/*EXPECT_SPAN_EVENTS
+/*EXPECT_SPAN_EVENTS_LIKE
 [
-  "?? agent run id",
-  {
-    "reservoir_size": 10000,
-    "events_seen": 4
-  },
-  [
     [
       {
         "traceId": "??",
@@ -45,23 +39,6 @@ newrelic.code_level_metrics.enabled=false
         "nr.entryPoint": true,
         "timestamp": "??",
         "transaction.name": "OtherTransaction\/php__FILE__"
-      },
-      {},
-      {}
-    ],
-    [
-      {
-        "traceId": "??",
-        "duration": "??",
-        "transactionId": "??",
-        "name": "Custom\/GuzzleHttp\\Client::__construct",
-        "guid": "??",
-        "type": "Span",
-        "category": "generic",
-        "priority": "??",
-        "sampled": true,
-        "parentId": "??",
-        "timestamp": "??"
       },
       {},
       {}
@@ -112,7 +89,6 @@ newrelic.code_level_metrics.enabled=false
         "http.statusCode": 200
       }
     ]
-  ]
 ]
 */
 
