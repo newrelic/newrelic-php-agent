8965a844709853b8fa889d3f564c5e5e2d1c99d2
Michael Fulbright <mfulbright@newrelic.com>
2023-11-20 15:02:31 -0500

    fix(tests): Adds code to check php packags

--- src/newrelic/integration/php_packages.go
+++ src/newrelic/integration/php_packages.go
@@ -6,6 +6,7 @@
 package integration
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -13,6 +14,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"slices"
+	"sort"
 	"strings"
 )
 
@@ -30,6 +32,7 @@ type PhpPackagesCollection struct {
 // PHP packages config describes how to collect the JSON for the packages installed
 // for the current test case
 type PhpPackagesConfiguration struct {
+	path                string
 	command             string
 	supported_list_file string
 }
@@ -46,26 +49,142 @@ type ComposerJSON struct {
 	Installed []ComposerPackage
 }
 
-// Create a package collection - requires config data from the EXPECT_PHP_PACKAGES stanza
-func NewPhpPackagesCollection(cfg []byte) *PhpPackagesCollection {
+//
+// Given a JSON harvest payload, extract the PHP packages
+//
+// Params 1 : JSON byte string containing upload_module_updates endpoint data
+//
+// Returns :  []PhpPackage with extracted package info, sorted by package name
+//            nil upon error processing JSON
+func GetPhpPackagesFromData(data []byte) ([]PhpPackage, error) {
+	var pkgs []PhpPackage
+	var x []interface{}
 
-	p := &PhpPackagesCollection{
-		config: PhpPackagesConfiguration{command: "composer show -f json", supported_list_file: "../../../include/supported_php_packages.json"},
+	// extract string "Jars" and array of Php Packages JSON
+	err := json.Unmarshal(data, &x)
+	fmt.Printf("initial x = %+v\n", x)
+	if nil != err {
+		return nil, err
+	}
+	if 2 != len(x) {
+		return nil, fmt.Errorf(("Expectd top level array of len 2"))
+	}
+	str, ok := x[0].(string)
+	if !ok || "Jars" != str {
+		return nil, fmt.Errorf(("Expected \"Jars\" string at top level"))
+	}
+	fmt.Printf("Passed first test\n")
+
+	// walk through array of Php packages JSON
+	v2, _ := x[1].([]interface{})
+	fmt.Printf("v2 = %+v\n", v2)
+	for i, e1 := range v2 {
+		v3, _ := e1.([]interface{})
+		if 3 != len(v3) {
+			return nil, fmt.Errorf("Expected php packages json to have 3 values, has %d : %+v", len(v3), v3)
+		}
+		fmt.Printf("pkg #%d: len = %d name = '%s' version = '%s'\n", i, len(v3), v3[0].(string), v3[1].(string))
+		pkgs = append(pkgs, PhpPackage{v3[0].(string), v3[1].(string)})
 	}
 
-	return p
+	// MSF TESTING
+	// add a few packages to test sorting/comparing
+	pkgs = append(pkgs, PhpPackage{"predis/predis", "3.5.0"})
+	pkgs = append(pkgs, PhpPackage{"guzzlehttp/guzzle", "1.2.9"})
+	//pkgs = append(pkgs, PhpPackage{"slim/slim", "5.0.7"})
+	// MSF TESTING
+
+	// sort by package name to aid comparision later
+	sort.Slice(pkgs, func(i, j int) bool {
+		return pkgs[i].Name < pkgs[j].Name
+	})
+
+	return pkgs, nil
 }
 
-// Given a configuration that defines how to find installed packages, detect
-// installed packages
-func (pkgs *PhpPackagesCollection) GatherInstalledPackages(path string) ([]PhpPackage, error) {
-	// if nil == pkgs {
-	// 	return fmt.Errorf("GatherInstallPackages(): pkgs is nil")
-	// }
+// convert PhpPackage to collector JSON representation
+func (pkg *PhpPackage) CollectorJSON() ([]byte, error) {
+	buf := &bytes.Buffer{}
+
+	buf.WriteString(`["`)
+	buf.WriteString(pkg.Name)
+	buf.WriteString(`","`)
+	buf.WriteString(pkg.Version)
+	buf.WriteString(`",{}]`)
+
+	return buf.Bytes(), nil
+}
+
+// Create a package collection - requires config data from the EXPECT_PHP_PACKAGES stanza
+func NewPhpPackagesCollection(path string, config []byte) (*PhpPackagesCollection, error) {
+
+	params := make(map[string]string)
+
+	for _, line := range bytes.Split(config, []byte("\n")) {
+		fmt.Printf("config line %s\n", string(line))
+		trimmed := bytes.TrimSpace(line)
+		if len(trimmed) == 0 {
+			continue
+		}
+
+		var key, value string
+
+		kv := bytes.SplitN(trimmed, []byte("="), 2)
+		key = string(bytes.TrimSpace(kv[0]))
+		if len(kv) == 2 {
+			value = string(bytes.TrimSpace(kv[1]))
+		}
+
+		if key == "" {
+			return nil, fmt.Errorf("invalid config format '%s'", string(config))
+		}
+
+		params[key] = value
+	}
+
+	fmt.Printf("params map = %+v\n", params)
+
+	// verify command and supported_list are defined
+	_, ok := params["command"]
+	fmt.Printf("ok 1 = %t\n", ok)
+	if ok {
+		_, ok = params["supported_packages"]
+		fmt.Printf("ok 2 = %t\n", ok)
+	}
+	fmt.Printf("ok final = %t\n", ok)
+
+	if !ok {
+		return nil, fmt.Errorf("Improper php applications config - must include 'command' and 'supported_packages' keys, got %+v", params)
+	}
+
+	p := &PhpPackagesCollection{
+		//config: PhpPackagesConfiguration{command: "composer show -f json", supported_list_file: "../../../include/supported_php_packages.json"},
+		config: PhpPackagesConfiguration{command: params["command"], path: path, supported_list_file: params["supported_packages"]},
+	}
+
+	fmt.Printf("Final p = %+v\n", p)
+
+	return p, nil
+}
+
+// Detects installed PHP packages
+//
+// Returns :  []PhpPackage with extracted package info, sorted by package name
+//            nil upon error processing JSON
+//
+// Notes   :  Currently only supports an application created with composer
+//
+func (pkgs *PhpPackagesCollection) GatherInstalledPackages() ([]PhpPackage, error) {
 
 	var err error
 
-	jsonFile, err := os.Open(filepath.Dir(path) + "/" + pkgs.config.supported_list_file)
+	fmt.Printf("pkgs = %+v\n", pkgs)
+
+	if nil == pkgs {
+		return nil, fmt.Errorf("GatherInstallPackages(): pkgs is nil")
+	}
+
+	jsonFile, err := os.Open(filepath.Dir(pkgs.config.path) + "/" + pkgs.config.supported_list_file)
 	if err != nil {
 		fmt.Println(err)
 		return nil, fmt.Errorf("error opening supported list %s", err.Error())
@@ -90,12 +209,12 @@ func (pkgs *PhpPackagesCollection) GatherInstalledPackages(path string) ([]PhpPa
 	}
 	fmt.Printf("supported len = %d contents = %+v\n", len(supported), supported)
 
-	pkgs = NewPhpPackagesCollection([]byte(""))
-	fmt.Printf("GatherInstalledPackages using path %s, config %+v\n", path, pkgs.config)
+	//pkgs = NewPhpPackagesCollection([]byte(""))
+	//fmt.Printf("GatherInstalledPackages using path %s, config %+v\n", path, pkgs.config)
 
 	splitCmd := strings.Split(pkgs.config.command, " ")
 	cmd := exec.Command(splitCmd[0], splitCmd[1:]...)
-	cmd.Dir = filepath.Dir(path)
+	cmd.Dir = filepath.Dir(pkgs.config.path)
 	cmd.Stderr = os.Stderr
 
 	var out []byte
@@ -104,12 +223,13 @@ func (pkgs *PhpPackagesCollection) GatherInstalledPackages(path string) ([]PhpPa
 	if nil != err {
 		fmt.Printf("Error from output command %s\n", err.Error())
 	}
-	fmt.Printf("The output from composer is %s\n", out)
+
+	//fmt.Printf("The output from composer is %s\n", out)
 
 	if "composer" == splitCmd[0] {
 		detected := ComposerJSON{}
 		json.Unmarshal([]byte(out), &detected)
-		fmt.Printf("detected len = %d contents = %+v\n", len(detected.Installed), detected)
+		//fmt.Printf("detected len = %d contents = %+v\n", len(detected.Installed), detected)
 		for _, v := range detected.Installed {
 			if slices.Contains(supported, v.Name) {
 				pkgs.packages = append(pkgs.packages, PhpPackage{v.Name, v.Version})
@@ -120,7 +240,35 @@ func (pkgs *PhpPackagesCollection) GatherInstalledPackages(path string) ([]PhpPa
 		return nil, fmt.Errorf("ERROR - unknown method '%s'\n", splitCmd[0])
 	}
 
-	fmt.Printf("pkgs.packages len = %d contexts = %+v\n", len(pkgs.packages), pkgs.packages)
+	// MSF TESTING
+	// add a few packages to test sorting/comparing
+	pkgs.packages = append(pkgs.packages, PhpPackage{"predis/predis", "3.4.0"})
+	pkgs.packages = append(pkgs.packages, PhpPackage{"guzzlehttp/guzzle", "1.2.3"})
+	// MSF TESTING
+
+	// sort by package name to aid comparision later
+	sort.Slice(pkgs.packages, func(i, j int) bool {
+		return pkgs.packages[i].Name < pkgs.packages[j].Name
+	})
+
+	//fmt.Printf("pkgs.packages len = %d contexts = %+v\n", len(pkgs.packages), pkgs.packages)
 
 	return pkgs.packages, nil
+}
+
+// convert PhpPackage to collector JSON representation
+func (pkgs *PhpPackagesCollection) CollectorJSON() ([]byte, error) {
+	buf := &bytes.Buffer{}
+
+	buf.WriteString(`["Jars",[`)
+	for i, pkg := range pkgs.packages {
+		json, _ := pkg.CollectorJSON()
+		buf.Write(json)
+		if i != (len(pkgs.packages) - 1) {
+			buf.WriteByte(',')
+		}
+	}
+	buf.WriteString(`]]`)
+
+	return buf.Bytes(), nil
 }
