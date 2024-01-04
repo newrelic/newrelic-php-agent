cbeb5ee3eea483d9ccdf7bb6dc0d7cd62f5d0f3d
ZNeumann <zneumann9@gmail.com>
2024-01-04 12:38:10 -0700

    Update agent/php_execute.c
    Co-authored-by: Amber Sistla <asistla@newrelic.com>

--- agent/php_execute.c
+++ agent/php_execute.c
@@ -2031,8 +2031,8 @@ static void nr_php_instrument_func_end(NR_EXECUTE_PROTO) {
     create_metric = wraprec->create_metric;
 
     /*
-     * A NULL return value means that there was an uncaught exception, and
-     * therefore we want to call the 'clean' function type
+     * A NULL return value ptr means that there was an uncaught exception
+     * and therefore we want to call the 'clean' function type
      */
     if (NULL != nr_php_get_return_value(NR_EXECUTE_ORIG_ARGS)) {
       zcaught = nr_zend_call_orig_execute_special(wraprec, segment,
