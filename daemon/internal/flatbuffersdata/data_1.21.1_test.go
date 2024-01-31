08fe3cf308ed2eb5c52c0cdbcfdf29bdeece4a59
Brian Duranleau <bduranleau@newrelic.com>
2024-01-31 17:37:20 -0600

    fix: update outdated go 1.7+1.8 tests

--- daemon/internal/flatbuffersdata/data_1.21.1_test.go
+++ daemon/internal/flatbuffersdata/data_1.21.1_test.go
@@ -3,15 +3,14 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-//go:build go1.8
-// +build go1.8
+//go:build go1.21.1
 
 package flatbuffersdata
 
 const testFlatbuffersTxnDataExpectedJSON = `[` +
 	`{"name":"Supportability/TxnData/CustomEvents","forced":true,"data":[1,3,0,3,3,9]},` +
 	`{"name":"Supportability/TxnData/Metrics","forced":true,"data":[1,2,0,2,2,4]},` +
-	`{"name":"Supportability/TxnData/Size","forced":true,"data":[1,1404,0,1404,1404,1971216]},` +
+	`{"name":"Supportability/TxnData/Size","forced":true,"data":[1,1408,0,1408,1408,1982464]},` +
 	`{"name":"Supportability/TxnData/SlowSQL","forced":true,"data":[1,1,0,1,1,1]},` +
 	`{"name":"Supportability/TxnData/TraceSize","forced":true,"data":[1,2,0,2,2,4]},` +
 	`{"name":"forced","forced":true,"data":[6,5,4,3,2,1]},` +
