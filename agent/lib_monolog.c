6355034731401e71ea96dbd03585ccf7e3f9f1ec
Hitesh Ahuja <hahuja@newrelic.com>
2024-02-01 10:21:43 -0800

    add php package to transaction for testing

--- agent/lib_monolog.c
+++ agent/lib_monolog.c
@@ -512,4 +512,5 @@ void nr_monolog_enable(TSRMLS_D) {
   nr_php_wrap_user_function(NR_PSTR("Monolog\\Logger::addRecord"),
                             nr_monolog_logger_addrecord TSRMLS_CC);
   nr_txn_add_php_package(NRPRG(txn), "monolog/monolog", "10.0.1.1");
+  nr_txn_add_php_package(NRPRG(txn), "laravel/framework", "8.83.27");
 }
