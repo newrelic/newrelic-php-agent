67c3f00e282eb2818fd163d30ca3111a47fb9f0c
Brian Duranleau <bduranleau@newrelic.com>
2024-02-06 15:13:19 -0600

    chore: add verbosedebug to outbound w3c ts header

--- axiom/nr_distributed_trace.c
+++ axiom/nr_distributed_trace.c
@@ -1211,6 +1211,10 @@ char* nr_distributed_trace_create_w3c_tracestate_header(
 
   nr_free(priority_buf);
 
+  nrl_verbosedebug(NRL_CAT,
+                   "Outbound W3C TraceState Context Header generated: %s",
+                   trace_context_header);
+
   return trace_context_header;
 }
 
