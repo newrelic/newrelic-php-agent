fc77d3daaa1faafa589f25f8855d6eace4a7ac98
Michael Fulbright <mfulbright@newrelic.com>
2024-02-12 16:21:27 -0500

    fix(agent): Improves wordpress package scanning
    - use PHP IIFE to call function in single zend_eval_string invocation
    - use array_key_exists() instead of isset()
    - return value on catch

--- agent/fw_wordpress.c
+++ agent/fw_wordpress.c
@@ -659,40 +659,30 @@ NR_PHP_WRAPPER_END
 
 void nr_wordpress_version() {
   char* func_string
-      = "if (!function_exists('newrelic_wordpress_get_version')) {"
-        "  function newrelic_wordpress_get_version() {"
-        "    try {"
-        "      if (isset($GLOBALS['wp_version'])) {"
-        "        return $GLOBALS['wp_version'];"
-        "      }"
-        "      else {"
-        "        return ' ';"
-        "      }"
-        "    } catch (Exception $e) {"
+      = ""
+        "(function() {"
+        "  try {"
+        "    if (array_key_exists('wp_version', $GLOBALS)) {"
+        "      return $GLOBALS['wp_version'];"
         "    }"
+        "    else {"
+        "      return ' ';"
+        "    }"
+        "  } catch (Exception $e) {"
+        "      return ' ';"
         "  }"
-        "}";
+        "})();";
 
   zval retval;
-  int result = zend_eval_string(func_string, NULL,
-                                "Define Wordpress Get Version Function");
-  if (SUCCESS != result) {
-    return;
-  }
-
-  zend_function* func = nr_php_find_function("newrelic_wordpress_get_version");
-
-  if (func) {
-    result = zend_eval_string("newrelic_wordpress_get_version();", &retval,
-                              "Get Wordpress Version");
-    // Add php package to transaction
-    if (SUCCESS == result) {
-      if (IS_STRING == Z_TYPE(retval)) {
-        char* version = Z_STRVAL(retval);
-        nr_txn_add_php_package(NRPRG(txn), "wordpress", version);
-      }
-      zval_dtor(&retval);
+  int result
+      = zend_eval_string(func_string, &retval, "Get Wordpress Version");
+  // Add php package to transaction
+  if (SUCCESS == result) {
+    if (IS_STRING == Z_TYPE(retval)) {
+      char* version = Z_STRVAL(retval);
+      nr_txn_add_php_package(NRPRG(txn), "wordpress", version);
     }
+    zval_dtor(&retval);
   }
 }
 
