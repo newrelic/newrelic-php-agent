fc5c9a23dffa0c4133369b336a2ee8533e3c2436
Michael Fulbright <mfulbright@newrelic.com>
2024-03-05 14:04:23 -0500

    fix(testing): Fixes tests for error linking changes
    The error linking changes added a "guid" intrinsic attribute to
    ErrorTrace and TransactionError JSON.  This patch addresses
    these changes.
    
    For tests in the "errors" directory the attributes are tested
    explicitely, while in the remaining integration tests which
    use EXPECT_TRACED_ERRORS or EXPECT_ERROR_EVENTS
    it was left as a wildcard match.  The rationale is the "errors"
    tests should provide adequate coverage to ensure the "guid"
    attribute is being generated properly.

--- tests/integration/errors/test_restore_top_level_exception_internal_function.php
+++ tests/integration/errors/test_restore_top_level_exception_internal_function.php
@@ -25,7 +25,17 @@ handler and restore_exception_handler is called.
           " in gamma called at __FILE__ (??)"
         ],
         "agentAttributes": "??",
-        "intrinsics": "??"
+        "intrinsics":
+        {
+          "totalTime": "??",
+          "cpu_time": "??",
+          "cpu_user_time": "??",
+          "cpu_sys_time": "??",
+          "guid": "??",
+          "sampled": "??",
+          "priority": "??",
+          "traceId": "??"
+        }
       },
       "?? transaction ID"
     ]
