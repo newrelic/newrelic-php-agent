a9656a8170fdb7bbd97823694d69ac08bf7f98a2
Zach Neumann <zneumann@newrelic.com>
2024-05-15 14:42:59 -0600

    TEMP remove metric

--- agent/php_execute.c
+++ agent/php_execute.c
@@ -2010,6 +2010,7 @@ void nr_php_observer_fcall_begin_late(zend_execute_data* execute_data, nrtime_t 
     return;
   }
 
+  if (false) {
   if (NR_OP_ARRAY->scope) {
       nr_txn_force_single_count(NRPRG(txn), nr_txn_create_fn_supportability_metric(
                                   nr_php_op_array_function_name(NR_OP_ARRAY),
@@ -2018,6 +2019,7 @@ void nr_php_observer_fcall_begin_late(zend_execute_data* execute_data, nrtime_t 
       nr_txn_force_single_count(NRPRG(txn), nr_txn_create_fn_supportability_metric(
                                   nr_php_op_array_function_name(NR_OP_ARRAY),
                                   NULL));
+  }
   }
   /*
    * Check for, and handle, frameworks.
