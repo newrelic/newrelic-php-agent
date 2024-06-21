1e5e37907cff21dd9dcab5a121679a5c821e3059
Michal Nowacki <mnowacki@newrelic.com>
2024-06-21 12:03:51 -0400

    expose app metadata for csec via global symbol
    Rather than providing a PHP user function to expose app metadata to PHP
    userland, export a global symbol for cseg agent to use.

--- agent/csec_metadata.c
+++ agent/csec_metadata.c
@@ -1,48 +1,37 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "csec_metadata.h"
-#include "util_strings.h"
-#include "php_hash.h"
-#include "php_api_internal.h"
 
-static void nr_csec_php_add_assoc_string_const(zval* arr,
-                                          const char* key,
-                                          const char* value) {
-  char* val = NULL;
+#include "util_memory.h"
 
-  if (NULL == arr || NULL == key || NULL == value) {
-    return;
+#include "nr_axiom.h"
+#include "nr_agent.h"
+#include "nr_app.h"
+#include "php_includes.h"
+#include "php_compat.h"
+#include "php_newrelic.h"
+
+int nr_php_csec_get_metadata(nr_php_csec_metadata_t* csec_metadata) {
+  if (NULL == csec_metadata) {
+    return -1;
   }
 
-  val = nr_strdup(value);
-  nr_php_add_assoc_string(arr, key, val);
-  nr_free(val);
-}
-
-#ifdef TAGS
-void zif_newrelic_get_security_metadata(void); /* ctags landing pad only */
-void newrelic_get_security_metadata(void);     /* ctags landing pad only */
-#endif
-PHP_FUNCTION(newrelic_get_security_metadata) {
-
-  NR_UNUSED_RETURN_VALUE;
-  NR_UNUSED_RETURN_VALUE_PTR;
-  NR_UNUSED_RETURN_VALUE_USED;
-  NR_UNUSED_THIS_PTR;
-  NR_UNUSED_EXECUTE_DATA;
-
-  array_init(return_value);
-
-  nr_csec_php_add_assoc_string_const(return_value, KEY_ENTITY_NAME, nr_app_get_entity_name(NRPRG(app)));
-  nr_csec_php_add_assoc_string_const(return_value, KEY_ENTITY_TYPE, nr_app_get_entity_type(NRPRG(app)));
-  nr_csec_php_add_assoc_string_const(return_value, KEY_ENTITY_GUID, nr_app_get_entity_guid(NRPRG(app)));
-  nr_csec_php_add_assoc_string_const(return_value, KEY_HOSTNAME, nr_app_get_host_name(NRPRG(app)));
-  nr_csec_php_add_assoc_string_const(return_value, KEY_LICENSE, NRPRG(license).value);
-
-  if (NRPRG(app)) {
-    nr_csec_php_add_assoc_string_const(return_value, KEY_AGENT_RUN_ID, NRPRG(app)->agent_run_id);
-    nr_csec_php_add_assoc_string_const(return_value, KEY_ACCOUNT_ID, NRPRG(app)->account_id);
-    nr_csec_php_add_assoc_string_const(return_value, KEY_PLICENSE, NRPRG(app)->plicense);
-    int high_security = NRPRG(app)->info.high_security;
-    add_assoc_long(return_value, KEY_HIGH_SECURITY, (long)high_security);
+  if (NULL == NRPRG(app)) {
+    return -2;
   }
 
+  csec_metadata->high_security = NRPRG(app)->info.high_security;
+  csec_metadata->entity_name = nr_strdup(nr_app_get_entity_name(NRPRG(app)));
+  csec_metadata->entity_type = nr_strdup(nr_app_get_entity_type(NRPRG(app)));
+  csec_metadata->entity_guid = nr_strdup(nr_app_get_entity_guid(NRPRG(app)));
+  csec_metadata->host_name = nr_strdup(nr_app_get_host_name(NRPRG(app)));
+  csec_metadata->agent_run_id = nr_strdup(NRPRG(app)->agent_run_id);
+  csec_metadata->account_id = nr_strdup(NRPRG(app)->account_id);
+  csec_metadata->license = nr_strdup(NRPRG(license).value);
+  csec_metadata->plicense = nr_strdup(NRPRG(app)->plicense);
+
+  return 0;
 }
--- agent/csec_metadata.h
+++ agent/csec_metadata.h
@@ -1,12 +1,35 @@
-#include "php_agent.h"
-#include "util_hashmap.h"
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-#define KEY_ENTITY_NAME "entity.name"
-#define KEY_ENTITY_TYPE "entity.type"
-#define KEY_ENTITY_GUID "entity.guid"
-#define KEY_HOSTNAME "hostname"
-#define KEY_AGENT_RUN_ID "agent.run.id"
-#define KEY_ACCOUNT_ID "account.id"
-#define KEY_LICENSE "license"
-#define KEY_PLICENSE "plicense"
-#define KEY_HIGH_SECURITY "high_security"
+#ifndef CSEC_METADATA_H
+#define CSEC_METADATA_H
+
+typedef struct _nr_php_csec_metadata_t {
+  int high_security;  /* Indicates if high security been set locally for this
+                         application */
+  char* license;      /* License key provided */
+  char* plicense;     /* Printable license (abbreviated for security) */
+  char* host_name;    /* Local host name reported to the daemon */
+  char* entity_name;  /* Entity name related to this application */
+  char* entity_type;  /* Entity type */
+  char* account_id;   /* Security : Added for getting account id */
+  char* entity_guid;  /* Entity guid related to this application */
+  char* agent_run_id; /* The collector's agent run ID; assigned from the
+                         New Relic backend */
+} nr_php_csec_metadata_t;
+
+/*
+ * Purpose : Return app meta data by populating nr_php_csec_metadata_t
+ *           structure. The caller is responsible for freeing the memory
+ *           allocated for the strings in the structure.
+ *
+ * Params  : Pointer to a nr_php_csec_metadata_t structure
+ *
+ * Returns : 0 for success
+ *          -1 for invalid input
+ *          -2 for invalid internal state
+ */
+extern int nr_php_csec_get_metadata(nr_php_csec_metadata_t*);
+#endif
