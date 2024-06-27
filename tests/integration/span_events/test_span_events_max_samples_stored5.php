e3a6bd618050351bb053e8889b6b5123ba4d6623
Zach Neumann <zneumann@newrelic.com>
2024-06-27 14:06:47 -0600

    fix test

--- tests/integration/span_events/test_span_events_max_samples_stored5.php
+++ tests/integration/span_events/test_span_events_max_samples_stored5.php
@@ -5,20 +5,20 @@
  */
 
 /*DESCRIPTION
-This is the max that can be sent with DT only.  With span limit < 7000,
+This is the max that can be sent with DT only.  With span limit > 6800,
 infinite tracing NEEDS to be enabled; otherwise, the daemon will
 error out with the following type message:
 `Error: listener: closing connection: maximum message size exceeded, (2886388 > 2097152)`
 The maximum without DT is 7200; however, it causes 
 the listener to slow down so much that it will cause intermittent failures on other samples_stored tests.
-Only 7000 span events must be sent when the limit is set to 7000.
+Only 6800 span events must be sent when the limit is set to 6800.
 */
 
 /*INI
 newrelic.distributed_tracing_enabled = 1
 newrelic.transaction_tracer.threshold = 0
 newrelic.cross_application_tracer.enabled = false
-newrelic.span_events.max_samples_stored = 7000
+newrelic.span_events.max_samples_stored = 6800
 newrelic.code_level_metrics.enabled=false
 */
 
@@ -27,7 +27,7 @@ newrelic.code_level_metrics.enabled=false
   "?? agent run id",
   {
     "reservoir_size": 10000,
-    "events_seen": 7000
+    "events_seen": 6800
   },
   "??"
 ]
