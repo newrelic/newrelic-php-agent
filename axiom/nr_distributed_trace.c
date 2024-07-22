3c754e3c5a1a623d635c6c6263d8393912694bf1
Zach Neumann <zneumann@newrelic.com>
2024-07-22 07:57:25 -0600

    typo

--- axiom/nr_distributed_trace.c
+++ axiom/nr_distributed_trace.c
@@ -501,7 +501,7 @@ void nr_distributed_trace_set_trace_id(nr_distributed_trace_t* dt,
         dt->trace_id = dest;
       } else {
         dt->trace_id = nr_strdup(trace_id);
-      {
+      }
     } else {
       dt->trace_id = nr_strdup(trace_id);
     }
