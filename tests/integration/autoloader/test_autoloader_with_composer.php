774b23389561d20ef83d87acdfade5d620797874
Michal Nowacki <mnowacki@newrelic.com>
2024-09-06 10:54:59 -0400

    add basic autoloader detection tests

---
+++ tests/integration/autoloader/test_autoloader_with_composer.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Test detection of autoloader when Composer is used. Supportability metrics for
+Autoloader and Composer libraries should be present.
+*/
+
+/*INI
+*/
+
+/*EXPECT_METRICS_EXIST
+Supportability/library/Autoloader/detected, 1
+Supportability/library/Composer/detected, 1
+*/
+
+
+require 'autoload-with-composer/vendor/autoload.php';
