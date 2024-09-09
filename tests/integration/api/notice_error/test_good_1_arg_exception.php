bfdbce1ac2f49d2b4ab47316ce1ead5b6eb29c7e
Michael Fulbright <mfulbright@newrelic.com>
2024-09-09 15:34:17 -0400

    chore(agent): Adds happy path newrelic_notice_error() tests

---
+++ tests/integration/api/notice_error/test_good_1_arg_exception.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+The agent should record a traced error when newrelic_notice_error is
+called with 1 argument which is an exception.
+*/
+
+/*EXPECT_TRACED_ERRORS
+[
+  "?? agent run id",
+  [
+    [
+      "?? when",
+      "OtherTransaction/php__FILE__",
+      "Noticed exception 'Exception' with message '1 arg exception' in __FILE__:??",
+      "Exception",
+      {
+        "stack_trace": [
+          " in a called at __FILE__ (??)"
+     ],
+        "agentAttributes": "??",
+        "intrinsics": "??"
+      },
+      "?? transaction ID"
+    ]
+  ]
+]
+*/
+
+/*EXPECT_ERROR_EVENTS
+[
+  "?? agent run id",
+  {
+    "reservoir_size": "??",
+    "events_seen": 1
+  },
+  [
+    [
+      {
+        "type": "TransactionError",
+        "timestamp": "??",
+        "error.class": "Exception",
+        "error.message": "Noticed exception 'Exception' with message '1 arg exception' in __FILE__:??",
+        "transactionName": "OtherTransaction\/php__FILE__",
+        "duration": "??",
+        "nr.transactionGuid": "??",
+        "guid": "??",
+        "sampled": true,
+        "priority": "??",
+        "traceId": "??",
+        "spanId": "??"
+      },
+      {},
+      {}
+    ]
+  ]
+]
+*/
+
+/*EXPECT_SPAN_EVENTS_LIKE
+[
+  [
+    {
+      "traceId": "??",
+      "duration": "??",
+      "transactionId": "??",
+      "name": "Custom\/a",
+      "guid": "??",
+      "type": "Span",
+      "category": "generic",
+      "priority": "??",
+      "sampled": true,
+      "timestamp": "??",
+      "parentId": "??"
+    },
+    {},
+    {
+      "error.message": "Noticed exception 'Exception' with message '1 arg exception' in __FILE__:??",
+      "error.class": "Exception",
+      "code.lineno": "??",
+      "code.filepath": "__FILE__",
+      "code.function": "a"
+    }
+  ]
+]
+*/
+
+require_once(realpath(dirname(__FILE__)) . '/../../../include/tap.php');
+
+function a()
+{
+  // One argument with an exception - this is tricky because most
+  // values are implicitly convertible to strings. Use a resource to prevent this.
+  newrelic_notice_error("1 arg", new Exception("1 arg exception"));
+}
+
+
+a();
---
+++ tests/integration/api/notice_error/test_good_5_args.php
@@ -0,0 +1,100 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+The agent should record a traced error when newrelic_notice_error is
+called with 5 parameters.
+*/
+
+/*EXPECT_TRACED_ERRORS
+[
+  "?? agent run id",
+  [
+    [
+      "?? when",
+      "OtherTransaction/php__FILE__",
+      "5 arg error",
+      "NoticedError",
+      {
+        "stack_trace": [
+          " in newrelic_notice_error called at __FILE__ (??)",
+          " in a called at __FILE__ (??)"
+        ],
+        "agentAttributes": "??",
+        "intrinsics": "??"
+      },
+      "?? transaction ID"
+    ]
+  ]
+]
+*/
+
+/*EXPECT_ERROR_EVENTS
+[
+  "?? agent run id",
+  {
+    "reservoir_size": "??",
+    "events_seen": 1
+  },
+  [
+    [
+      {
+        "type": "TransactionError",
+        "timestamp": "??",
+        "error.class": "NoticedError",
+        "error.message": "5 arg error",
+        "transactionName": "OtherTransaction\/php__FILE__",
+        "duration": "??",
+        "nr.transactionGuid": "??",
+        "guid": "??",
+        "sampled": true,
+        "priority": "??",
+        "traceId": "??",
+        "spanId": "??"
+      },
+      {},
+      {}
+    ]
+  ]
+]
+*/
+
+/*EXPECT_SPAN_EVENTS_LIKE
+[
+  [
+    {
+      "traceId": "??",
+      "duration": "??",
+      "transactionId": "??",
+      "name": "Custom\/a",
+      "guid": "??",
+      "type": "Span",
+      "category": "generic",
+      "priority": "??",
+      "sampled": true,
+      "timestamp": "??",
+      "parentId": "??"
+    },
+    {},
+    {
+      "error.message": "5 arg error",
+      "error.class": "NoticedError",
+      "code.lineno": "??",
+      "code.filepath": "__FILE__",
+      "code.function": "a"
+    }
+  ]
+]
+*/
+
+require_once(realpath(dirname( __FILE__ )) . '/../../../include/tap.php');
+
+function a() {
+  // Five argument form requires second arg to be convertible to a string.
+  newrelic_notice_error(42, "5 arg error", "file", __LINE__, "string");
+}
+
+a();
