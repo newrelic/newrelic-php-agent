cde843bbcfb45fd9c099d9b2034d811cd0bbe3e3
Michael Fulbright <mfulbright@newrelic.com>
2024-09-09 16:45:05 -0400

    feat(agent): Adds newrelic_notice_error tests as handler

---
+++ tests/integration/api/notice_error/test_good_4_args_error_handler.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+The agent should record a traced error when newrelic_notice_error is
+called with 4 parameters.
+*/
+
+/*EXPECT_TRACED_ERRORS
+[
+  "?? agent run id",
+  [
+    [
+      "?? when",
+      "OtherTransaction/php__FILE__",
+      "4 arg error",
+      "NoticedError",
+      {
+        "stack_trace": [
+          " in newrelic_notice_error called at ? (?)",
+          " in trigger_error called at __FILE__ (??)",
+          " in a called at __FILE__ (??)"
+        ],
+        "agentAttributes": "??",
+        "intrinsics": "??"
+      },
+      "?? transaction ID"
+    ]
+  ]
+]
+*/
+
+/*EXPECT_ERROR_EVENTS
+[
+  "?? agent run id",
+  {
+    "reservoir_size": "??",
+    "events_seen": 1
+  },
+  [
+    [
+      {
+        "type": "TransactionError",
+        "timestamp": "??",
+        "error.class": "NoticedError",
+        "error.message": "4 arg error",
+        "transactionName": "OtherTransaction\/php__FILE__",
+        "duration": "??",
+        "nr.transactionGuid": "??",
+        "guid": "??",
+        "sampled": true,
+        "priority": "??",
+        "traceId": "??",
+        "spanId": "??"
+      },
+      {},
+      {}
+    ]
+  ]
+]
+*/
+
+/*EXPECT_SPAN_EVENTS_LIKE
+[
+  [
+    {
+      "traceId": "??",
+      "duration": "??",
+      "transactionId": "??",
+      "name": "OtherTransaction\/php__FILE__",
+      "guid": "??",
+      "type": "Span",
+      "category": "generic",
+      "priority": "??",
+      "sampled": true,
+      "timestamp": "??",
+      "nr.entryPoint": true,
+      "transaction.name": "OtherTransaction\/php__FILE__"
+    },
+    {},
+    {
+    }
+  ],
+  [
+    {
+      "traceId": "??",
+      "duration": "??",
+      "transactionId": "??",
+      "name": "Custom\/a",
+      "guid": "??",
+      "type": "Span",
+      "category": "generic",
+      "priority": "??",
+      "sampled": true,
+      "timestamp": "??",
+      "parentId": "??"
+    },
+    {},
+    {
+      "error.message": "4 arg error",
+      "error.class": "NoticedError",
+      "code.lineno": "??",
+      "code.filepath": "__FILE__",
+      "code.function": "a"
+    }
+  ]
+]
+*/
+
+require_once(realpath(dirname( __FILE__ )) . '/../../../include/tap.php');
+
+function a() {
+    trigger_error("4 arg error", E_USER_ERROR);
+}
+
+set_error_handler('newrelic_notice_error', E_USER_ERROR);
+a();
