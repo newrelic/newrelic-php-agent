291930c2d86cb1aa92b706d95dc4be87f5c64723
Michal Nowacki <mnowacki@newrelic.com>
2024-09-12 15:53:18 -0400

    adjust list of magic files
    vendor/composer/installed.php is critical component of Composer's runtime API -
    methods from \Composer\InstalledVersions class do not work without it. Include
    this file in the mocked composer of 'happy path' tests.

---
+++ tests/integration/autoloader/autoload-with-composer/vendor/composer/installed.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+This file is needed by real Composer's runtime API, and agent verifies its presence to determine if Composer is used.
+Therefore, even though mocked version of Composer's runtime API does not use this file but (and that's why its empty),
+it still needs to be present.
+*/
