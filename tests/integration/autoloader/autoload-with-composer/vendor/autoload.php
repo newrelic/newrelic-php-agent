41f8ecbf1569f6c573752f8a3d287716049c5ff7
Michal Nowacki <mnowacki@newrelic.com>
2024-09-12 16:01:22 -0400

    add copyright and description to autoloaders mocks

--- tests/integration/autoloader/autoload-with-composer/vendor/autoload.php
+++ tests/integration/autoloader/autoload-with-composer/vendor/autoload.php
@@ -1,1 +1,12 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+This file is the usual entry point for installing and using PSR-4 Autoloader.
+The agent verifies its presence to determine if PSR-4 Autoloader is used.
+*/
+
 echo "";
