a7e981b0af463a76af29a6dd9aac31648e822cee
Michal Nowacki <mnowacki@newrelic.com>
2024-09-16 18:45:00 -0400

    improve integration tests
    Make mock of Composer more realistic:
    - ./vendor/autoload.php actually requires ./composer/autoload_real.php
    - \Composer\InstalledVersions actually depends on installed.php
    - installed.php contents is closer to real life

--- tests/integration/autoloader/autoload-with-composer/vendor/composer/installed.php
+++ tests/integration/autoloader/autoload-with-composer/vendor/composer/installed.php
@@ -6,6 +6,26 @@
 
 /*DESCRIPTION
 This file is needed by real Composer's runtime API, and agent verifies its presence to determine if Composer is used.
-Therefore, even though mocked version of Composer's runtime API does not use this file but (and that's why its empty),
-it still needs to be present.
+This file contains phpized version of composer.json for the project and its dependencies.
 */
+  return array(
+    // Mocked data: root package
+    'root' => array(
+        'pretty_version' => 'v1.0.0',
+        'version' => '1.0.0.0',
+        'type' => 'project'
+    ),
+    // Mocked data: installed packages and their versions
+    'versions' =>  array(
+        'vendor1/package1' => array(
+            'pretty_version' => 'v1.1.3',
+            'version' => '1.1.3.0',
+            'type' => 'library'
+        ),
+        'vendor2/package2' => array(
+            'pretty_version' => '2.1.5',
+            'version' => '2.1.5.0',
+            'type' => 'library'
+            )
+    )
+  );
