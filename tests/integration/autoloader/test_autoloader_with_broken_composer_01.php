c0139976440e6b085a64005cad24ac7f3b42c1be
Michal Nowacki <mnowacki@newrelic.com>
2024-09-19 23:59:05 -0400

    add autoloader tests with broken composer install

---
+++ tests/integration/autoloader/test_autoloader_with_broken_composer_01.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Test detection of autoloader when Composer is used but Composer installation
+is broken - class is unavailable. Supportability metric for Autoloader
+library should be present but not for Composer library. Package harvest should
+be empty. No errors should be generated.
+*/
+
+/*INI
+newrelic.vulnerability_management.composer_api.enabled=true
+*/
+
+/*EXPECT_PHP_PACKAGES
+null
+*/
+
+/*EXPECT_METRICS_EXIST
+Supportability/library/Autoloader/detected, 1
+*/
+
+/*EXPECT_METRICS_DONT_EXIST
+Supportability/library/Composer/detected, 1
+*/
+
+/*EXPECT_TRACED_ERRORS null*/
+
+require 'autoload-with-broken-composer-01/vendor/autoload.php';
