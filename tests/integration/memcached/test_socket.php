a78bcf4c8dcda8f6f1fb86b48cd3f6da5a592c6f
Zach Neumann <zneumann@newrelic.com>
2024-09-27 11:22:52 -0400

    modify socket test

--- tests/integration/memcached/test_socket.php
+++ tests/integration/memcached/test_socket.php
@@ -16,25 +16,11 @@ added at once
 /*INI
 */
 
-/*EXPECT_METRICS
-[
-  "?? agent run id",
-  "?? start time",
-  "?? stop time",
-  [
-    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/all"}, [1, "??", "??", "??", "??", "??"]],
-    [{"name":"DurationByCaller/Unknown/Unknown/Unknown/Unknown/allOther"}, [1, "??", "??", "??", "??", "??"]],
-    [{"name":"Datastore/instance/Memcached/__HOST__/my_socket"},      [1, "??", "??", "??", "??", "??"]],
-    [{"name":"OtherTransaction/all"},                                 [1, "??", "??", "??", "??", "??"]],
-    [{"name":"OtherTransaction/php__FILE__"},                         [1, "??", "??", "??", "??", "??"]],
-    [{"name":"OtherTransactionTotalTime"},                            [1, "??", "??", "??", "??", "??"]],
-    [{"name":"OtherTransactionTotalTime/php__FILE__"},                [1, "??", "??", "??", "??", "??"]],
-    [{"name":"Supportability/Logging/Forwarding/PHP/enabled"},        [1, "??", "??", "??", "??", "??"]],
-    [{"name":"Supportability/Logging/Metrics/PHP/enabled"},           [1, "??", "??", "??", "??", "??"]],
-    [{"name":"Supportability/Logging/LocalDecorating/PHP/disabled"},  [1, "??", "??", "??", "??", "??"]]
-  ]
-]
+/*EXPECT_METRICS_EXIST
+Datastore/instance/Memcached/__HOST__/my_socket, 1
 */
+
+/*EXPECT_ERROR_EVENTS null */
 
 require_once(realpath (dirname ( __FILE__ )) . '/../../include/helpers.php');
 require_once(realpath (dirname ( __FILE__ )) . '/../../include/tap.php');
