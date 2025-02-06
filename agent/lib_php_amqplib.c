90da3b7c75ffb66a1d1548f89231d44f36edf022
Amber Sistla <asistla@newrelic.com>
2025-02-05 21:28:51 -0700

    fix(agent): use zval is integer function

--- agent/lib_php_amqplib.c
+++ agent/lib_php_amqplib.c
@@ -190,7 +190,7 @@ static inline void nr_php_amqplib_get_host_and_port(
 
   amqp_port = nr_php_zend_hash_index_find(
       Z_ARRVAL_P(connect_constructor_params), AMQP_CONSTRUCT_PARAMS_PORT_INDEX);
-  if (IS_LONG == Z_TYPE_P((amqp_port))) {
+  if (nr_php_is_zval_valid_integer(amqp_port)) {
     message_params->server_port = Z_LVAL_P(amqp_port);
   }
 }
