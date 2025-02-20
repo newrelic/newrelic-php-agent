5426df7051bfd57451a2db74892dc30b1de929a1
Michael Fulbright <mfulbright@newrelic.com>
2025-02-20 16:44:58 -0500

    tests(daemon): Tests invalid data passed to SetLogForwardingLabels

--- daemon/internal/newrelic/log_events_test.go
+++ daemon/internal/newrelic/log_events_test.go
@@ -83,3 +83,76 @@ func TestSetLogForwardingLabels(t *testing.T) {
 		t.Errorf("expected JSON %s, got %s", expected, string(json))
 	}
 }
+
+func TestSetLogForwardingLabelsInvalidData(t *testing.T) {
+	events := NewLogEvents(10)
+	id := AgentRunID(`12345`)
+	log_data := []byte(`{"message":"test log event"}`)
+	label_data := []byte(`NOT VALID JSON`)
+	priority := SamplingPriority(0.5)
+
+	events.AddEventFromData(log_data, priority)
+	events.SetLogForwardingLabels(label_data)
+
+	if events.analyticsEvents.events.Len() != 1 {
+		t.Errorf("expected 1 event, got %d", events.analyticsEvents.events.Len())
+	}
+
+	es := *events.analyticsEvents.events
+	event := es[0]
+
+	if string(event.data) != string(log_data) {
+		t.Errorf("expected event data %s, got %s", string(log_data), string(event.data))
+	}
+
+	if event.priority != priority {
+		t.Errorf("expected event priority %f, got %f", priority, event.priority)
+	}
+
+	json, err := events.CollectorJSON(id)
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	expected := `[{"common": {"attributes": {}},` +
+		`"logs": [{"message":"test log event"}]}]`
+	if string(json) != expected {
+		t.Errorf("expected JSON %s, got %s", expected, string(json))
+	}
+}
+
+func TestSetLogForwardingLabelsNilData(t *testing.T) {
+	events := NewLogEvents(10)
+	id := AgentRunID(`12345`)
+	log_data := []byte(`{"message":"test log event"}`)
+	priority := SamplingPriority(0.5)
+
+	events.AddEventFromData(log_data, priority)
+	events.SetLogForwardingLabels(nil)
+
+	if events.analyticsEvents.events.Len() != 1 {
+		t.Errorf("expected 1 event, got %d", events.analyticsEvents.events.Len())
+	}
+
+	es := *events.analyticsEvents.events
+	event := es[0]
+
+	if string(event.data) != string(log_data) {
+		t.Errorf("expected event data %s, got %s", string(log_data), string(event.data))
+	}
+
+	if event.priority != priority {
+		t.Errorf("expected event priority %f, got %f", priority, event.priority)
+	}
+
+	json, err := events.CollectorJSON(id)
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	expected := `[{"common": {"attributes": {}},` +
+		`"logs": [{"message":"test log event"}]}]`
+	if string(json) != expected {
+		t.Errorf("expected JSON %s, got %s", expected, string(json))
+	}
+}
