95f0d6c805ef63b3e8bdd9e3e5d160d0f7346d10
Michael Fulbright <mfulbright@newrelic.com>
2025-02-24 16:20:41 -0500

    tests(agent): Updates integration/pgsql tests to have log labels metric

--- tests/integration/pgsql/test_pg_query_error.php
+++ tests/integration/pgsql/test_pg_query_error.php
@@ -48,7 +48,8 @@ newrelic.distributed_tracing_enabled=0
     [{"name":"OtherTransactionTotalTime/php__FILE__"},              [1, "??", "??", "??", "??", "??"]],
     [{"name":"Supportability/Logging/Forwarding/PHP/enabled"},      [1, "??", "??", "??", "??", "??"]],
     [{"name":"Supportability/Logging/Metrics/PHP/enabled"},         [1, "??", "??", "??", "??", "??"]],
-    [{"name":"Supportability/Logging/LocalDecorating/PHP/disabled"},[1, "??", "??", "??", "??", "??"]]
+    [{"name":"Supportability/Logging/LocalDecorating/PHP/disabled"},[1, "??", "??", "??", "??", "??"]],
+    [{"name":"Supportability/Logging/Labels/PHP/disabled"},         [1, "??", "??", "??", "??", "??"]]
   ]
 ]
 */
