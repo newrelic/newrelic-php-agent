b4b8d4f2b42bde688df4b7cee6f15bd99d3391cc
ZNeumann <zneumann@newrelic.com>
2025-03-03 14:22:46 -0700

    Update agent/lib_aws_sdk_php.h
    Co-authored-by: Amber Sistla <asistla@newrelic.com>

--- agent/lib_aws_sdk_php.h
+++ agent/lib_aws_sdk_php.h
@@ -83,7 +83,7 @@ extern void nr_lib_aws_sdk_php_sqs_handle(nr_segment_t* segment,
  *
  * Returns :
  *
- * Note: The caller is responsible for freeing cloud_atters->cloud_resource_id
+ * Note: The caller is responsible for freeing cloud_attrs->cloud_resource_id
  */
 void nr_aws_sdk_lambda_client_invoke_parse_args(NR_EXECUTE_PROTO, nr_segment_cloud_attrs_t* cloud_attrs);
 
