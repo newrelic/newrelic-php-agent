0836487a42ccc5fae16f9d4e267f0651931965ec
Brian Duranleau <bduranleau@newrelic.com>
2025-03-03 11:30:37 -0600

    test: add valid drupal hookmap test

---
+++ tests/integration/frameworks/drupal/test_hook_implementations_map_valid.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*DESCRIPTION
+Verify agent behavior on valid hookImplementationsMap
+*/
+
+/*INI
+newrelic.framework = drupal8
+*/
+
+/*EXPECT_TRACED_ERRORS null */
+
+/*EXPECT_ERROR_EVENTS null */
+
+/*EXPECT
+*/
+
+require_once __DIR__ . '/mock_module_handler_valid.php';
+
+// This specific API is needed for us to instrument the ModuleHandler
+class Drupal
+{
+  public function moduleHandler()
+  {
+    return new Drupal\Core\Extension\ModuleHandler();
+  }
+}
+
+// Create module handler
+$drupal = new Drupal();
+$handler = $drupal->moduleHandler();
