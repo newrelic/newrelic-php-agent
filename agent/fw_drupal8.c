faefa68416ac03ac6ba5ac7594251fbd9e5c53d6
Brian Duranleau <bduranleau@newrelic.com>
2025-05-08 10:03:00 -0500

    rename validation fn

--- agent/fw_drupal8.c
+++ agent/fw_drupal8.c
@@ -608,7 +608,8 @@ NR_PHP_WRAPPER(nr_drupal94_invoke_all_with_clean) {
 NR_PHP_WRAPPER_END
 #endif  // OAPI
 
-static bool nr_validate_key_val_arr(nr_php_string_hash_key_t* key, zval* val) {
+static bool nr_is_invalid_key_val_arr(nr_php_string_hash_key_t* key,
+                                      zval* val) {
   if (NULL == key || 0 == ZEND_STRING_LEN(key)
       || 0 == nr_php_is_zval_valid_array(val)
       || 0 == zend_hash_num_elements(Z_ARRVAL_P(val))) {
@@ -649,14 +650,14 @@ static bool nr_drupal_hook_attribute_instrument(zval* module_handler) {
 
   ZEND_HASH_FOREACH_STR_KEY_VAL(Z_ARRVAL_P(hook_implementation_map), hook_key,
                                 hook_val) {
-    if (nr_validate_key_val_arr(hook_key, hook_val)) {
+    if (nr_is_invalid_key_val_arr(hook_key, hook_val)) {
       nrl_warning(NRL_FRAMEWORK,
                   "hookImplementationsMap[hook]: invalid key or value");
       return false;
     }
 
     ZEND_HASH_FOREACH_STR_KEY_VAL(Z_ARRVAL_P(hook_val), class_key, class_val) {
-      if (nr_validate_key_val_arr(class_key, class_val)) {
+      if (nr_is_invalid_key_val_arr(class_key, class_val)) {
         nrl_warning(NRL_FRAMEWORK,
                     "hookImplementationsMap[class]: invalid key or value");
         return false;
