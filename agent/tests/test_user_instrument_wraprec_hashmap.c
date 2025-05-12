830445b23bde0d3e0a4dbdf7d6f4f3418da1251f
Michal Nowacki <mnowacki@newrelic.com>
2025-05-12 11:07:40 -0400

    ensure passing NULL for is_new_wraprec doesn't blow up

--- agent/tests/test_user_instrument_wraprec_hashmap.c
+++ agent/tests/test_user_instrument_wraprec_hashmap.c
@@ -23,73 +23,89 @@ static void test_wraprecs_hashmap() {
   nruserfn_t *wraprec, *found_wraprec;
   zend_string *func_name, *scope_name, *method_name;
   bool is_new_wraprec = false;
+  bool *is_new_wraprec_tests[] = {NULL, &is_new_wraprec};
 
   func_name = zend_string_init(NR_PSTR(FUNCTION_NAME), 0);
   scope_name = zend_string_init(NR_PSTR(SCOPE_NAME), 0);
   method_name = zend_string_init(NR_PSTR(METHOD_NAME), 0);
 
-  // user_instrument_wraprec_hashmap is initialized at minit
-  // destroy it to test agent's behavior when it is not initialized
-  nr_php_user_instrument_wraprec_hashmap_destroy();
+  for (size_t i = 0; i < sizeof(is_new_wraprec_tests) / sizeof(is_new_wraprec_tests[0]); i++) {
+    bool *is_new_wraprec_ptr = is_new_wraprec_tests[i];
+    // user_instrument_wraprec_hashmap is initialized at minit
+    // destroy it to test agent's behavior when it is not initialized
+    nr_php_user_instrument_wraprec_hashmap_destroy();
 
-  // Test valid operations before initializing the hashmap
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_null("adding valid function before init", wraprec);
-  tlib_pass_if_false("adding valid function before init", is_new_wraprec,
-                     "expected false for is_new_wraprec");
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_null("adding valid method before init", wraprec);
-  tlib_pass_if_false("adding valid function before init", is_new_wraprec,
-                     "expected false for is_new_wraprec");
+    // Test valid operations before initializing the hashmap
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_null("adding valid function before init", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+    tlib_pass_if_false("adding valid function before init", *is_new_wraprec_ptr,
+                      "expected false for is_new_wraprec");
+    }
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_null("adding valid method before init", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+    tlib_pass_if_false("adding valid function before init", *is_new_wraprec_ptr,
+                      "expected false for is_new_wraprec");
+    }
 
-  // Initialize the hashmap
-  nr_php_user_instrument_wraprec_hashmap_init();
+    // Initialize the hashmap
+    nr_php_user_instrument_wraprec_hashmap_init();
 
-  // Test valid operations after initializing the hashmap
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_not_null("adding valid global function", wraprec);
-  tlib_pass_if_true("adding valid global function", is_new_wraprec,
-                    "expected true for is_new_wraprec");
+    // Test valid operations after initializing the hashmap
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_not_null("adding valid global function", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+      tlib_pass_if_true("adding valid global function", *is_new_wraprec_ptr,
+                        "expected true for is_new_wraprec");
+    }
 
-  found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(func_name, NULL);
-  tlib_pass_if_ptr_equal("getting valid global function", wraprec, found_wraprec);
+    found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(func_name, NULL);
+    tlib_pass_if_ptr_equal("getting valid global function", wraprec, found_wraprec);
 
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_not_null("adding valid global function one more time", wraprec);
-  tlib_pass_if_false("adding valid global function one more time", is_new_wraprec,
-                    "expected false for is_new_wraprec");
-  tlib_pass_if_ptr_equal("getting valid global function one more time", wraprec, found_wraprec);
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(FUNCTION_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_not_null("adding valid global function one more time", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+      tlib_pass_if_false("adding valid global function one more time", *is_new_wraprec_ptr,
+                        "expected false for is_new_wraprec");
+    }
+    tlib_pass_if_ptr_equal("getting valid global function one more time", wraprec, found_wraprec);
 
-  found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(func_name, scope_name);
-  tlib_pass_if_null("getting global function with scope", found_wraprec);
+    found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(func_name, scope_name);
+    tlib_pass_if_null("getting global function with scope", found_wraprec);
 
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_not_null("adding valid scoped method", wraprec);
-  tlib_pass_if_true("adding valid scoped function", is_new_wraprec,
-                    "expected true for is_new_wraprec");
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_not_null("adding valid scoped method", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+      tlib_pass_if_true("adding valid scoped function", *is_new_wraprec_ptr,
+                        "expected true for is_new_wraprec");
+    }
 
-  found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(method_name, scope_name);
-  tlib_pass_if_ptr_equal("getting scoped method", wraprec, found_wraprec);
+    found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(method_name, scope_name);
+    tlib_pass_if_ptr_equal("getting scoped method", wraprec, found_wraprec);
 
-  wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
-                                                       &is_new_wraprec);
-  tlib_pass_if_not_null("adding valid scoped method one more time", wraprec);
-  tlib_pass_if_false("adding valid scoped method one more time", is_new_wraprec,
-                    "expected false for is_new_wraprec");
-  tlib_pass_if_ptr_equal("getting valid scoped method one more time", wraprec, found_wraprec);
+    wraprec = nr_php_user_instrument_wraprec_hashmap_add(NR_PSTR(SCOPED_METHOD_NAME),
+                                                        is_new_wraprec_ptr);
+    tlib_pass_if_not_null("adding valid scoped method one more time", wraprec);
+    if (NULL != is_new_wraprec_ptr) {
+      tlib_pass_if_false("adding valid scoped method one more time", *is_new_wraprec_ptr,
+                        "expected false for is_new_wraprec");
+    }
+    tlib_pass_if_ptr_equal("getting valid scoped method one more time", wraprec, found_wraprec);
 
-  found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(NULL, scope_name);
-  tlib_pass_if_null("getting scoped method without method name", found_wraprec);
+    found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(NULL, scope_name);
+    tlib_pass_if_null("getting scoped method without method name", found_wraprec);
 
-  found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(method_name, NULL);
-  tlib_pass_if_null("getting scoped method without scope", found_wraprec);
+    found_wraprec = nr_php_user_instrument_wraprec_hashmap_get(method_name, NULL);
+    tlib_pass_if_null("getting scoped method without scope", found_wraprec);
 
-  nr_php_user_instrument_wraprec_hashmap_destroy();
+    nr_php_user_instrument_wraprec_hashmap_destroy();
+  }
 
   zend_string_free(func_name);
   zend_string_free(scope_name);
