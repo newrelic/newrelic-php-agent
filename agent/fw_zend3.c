6dc8c99bac4c6425e0e7160ec43cc392e68093eb
Hitesh Ahuja <108540135+hahuja2@users.noreply.github.com>
2025-05-23 10:18:14 -0700

    chore(agent): remove zend 1.x/2.x and refactor to only support zend 3.x (#1064)
    This PR removes Zend 1.x/2.x support and refactors the existing
    instrumentation to only support Zend 3.x

--- agent/fw_zend3.c
+++ agent/fw_zend3.c
@@ -14,37 +14,41 @@
 #include "util_memory.h"
 
 /*
- * How ZF2 Routing Works
- * =====================
- * ZF2's has a Zend\Mvc\Router that decides which controller to call.
+ * Zend 3.x has a Zend\\Router
+ * that decides which controller to call.
  *
- * Config is done in module.config.php (which exists per-module), which is a
+ * Config is done in `module/Application/config/module.config.php`
+ * (which exists per-module), which is a
  * PHP file returning an associative array containing something that looks like
- * this example from the ZF2 Skeleton App:
+ * this example from the Zend Skeleton App tutorial
+ * https://docs.zendframework.com/tutorials/getting-started/routing-and-controllers/.
  *
- *   'router' => array(
- *     'routes' => array(
- *       'album' => array(
- *           'type'    => 'segment',
- *           'options' => array(
- *               'route'    => '/album[/:action][/:id]',
- *               'constraints' => array(
- *                   'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
- *                   'id'     => '[0-9]+',
- *               ),
- *               'defaults' => array(
- *                   'controller' => 'Album\Controller\Album',
- *                   'action'     => 'index',
- *               ),
- *           ),
+ *   'router' => [
+ *        'routes' => [
+ *            'album' => [
+ *                'type'    => Segment::class,
+ *                'options' => [
+ *                    'route' => '/album[/:action[/:id]]',
+ *                   'constraints' => [
+ *                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+ *                        'id'     => '[0-9]+',
+ *                    ],
+ *                    'defaults' => [
+ *                        'controller' => Controller\AlbumController::class,
+ *                        'action'     => 'index',
+ *                    ],
+ *                ],
+ *            ],
+ *        ],
+ *    ],
+ *
  *    ...
  *
  * Here, 'album' is the name of a route, and maps to some controller there is
  * an onRoute event that corresponds to making routing happen. We would
  * probably like to have some instrumentation of the type of actions that a
  * controller executes if the action is something like 'view' or 'list' or
- * 'edit', but 'id' is likely to be sensitive, and mucking about in
- * user-defined parameters is no fun anyways, so all we get is the route name.
+ * 'edit', but 'id' is likely to be sensitive, so all we get is the route name.
  *
  * One approach would be to instrument the onRoute event; we ended up going
  * with the setMatchedRouteName instead and just setting the path whenever that
@@ -57,14 +61,22 @@
  * presumably that was some optimization due to the return value not being used.
  */
 
-NR_PHP_WRAPPER(nr_zend2_name_the_wt) {
+/*
+ * txn naming scheme:
+ * In this case, `nr_txn_set_path` is called after `NR_PHP_WRAPPER_CALL` with
+ * `NR_OK_TO_OVERWRITE` and as this corresponds to calling the wrapped function
+ * in func_end no change is needed to ensure OAPI compatibility as it will use
+ * the default func_end after callback. This entails that the first wrapped
+ * function call of this type gets to name the txn.
+ */
+NR_PHP_WRAPPER(nr_zend3_name_the_wt) {
   zval* path = NULL;
   zval* this_var = NULL;
 
   (void)wraprec;
   NR_UNUSED_SPECIALFN;
 
-  NR_PHP_WRAPPER_REQUIRE_FRAMEWORK(NR_FW_ZEND2);
+  NR_PHP_WRAPPER_REQUIRE_FRAMEWORK(NR_FW_ZEND3);
 
   this_var = nr_php_scope_get(NR_EXECUTE_ORIG_ARGS TSRMLS_CC);
   if (0
@@ -83,7 +95,7 @@ NR_PHP_WRAPPER(nr_zend2_name_the_wt) {
   if (nr_php_is_zval_valid_string(path)) {
     char* path_term = nr_strndup(Z_STRVAL_P(path), Z_STRLEN_P(path));
 
-    nr_txn_set_path("Zend2", NRPRG(txn), path_term, NR_PATH_TYPE_ACTION,
+    nr_txn_set_path("Zend3", NRPRG(txn), path_term, NR_PATH_TYPE_ACTION,
                     NR_OK_TO_OVERWRITE);
     nr_free(path_term);
   } else {
@@ -99,36 +111,14 @@ leave:
 }
 NR_PHP_WRAPPER_END
 
-void nr_fw_zend2_enable(TSRMLS_D) {
-  nr_txn_set_path("Zend2", NRPRG(txn), "unknown", NR_PATH_TYPE_ACTION,
+void nr_fw_zend3_enable(TSRMLS_D) {
+  nr_txn_set_path("Zend3", NRPRG(txn), "unknown", NR_PATH_TYPE_ACTION,
                   NR_OK_TO_OVERWRITE);
 
-  /*
-   * We instrument all three of these. The Console one is used for
-   * ZF2 Console requests
-   * (http://framework.zend.com/manual/2.3/en/modules/zend.console.routes.html).
-   * HTTP and Console both inherit from the third, so it's unlikely
-   * that that method will be called unless someone is using custom
-   * routing.
-   */
-  nr_php_wrap_user_function(
-      NR_PSTR("Zend\\Mvc\\Router\\HTTP\\RouteMatch::setMatchedRouteName"),
-      nr_zend2_name_the_wt TSRMLS_CC);
-  nr_php_wrap_user_function(
-      NR_PSTR("Zend\\Mvc\\Router\\Console\\RouteMatch::setMatchedRouteName"),
-      nr_zend2_name_the_wt TSRMLS_CC);
-  nr_php_wrap_user_function(
-      NR_PSTR("Zend\\Mvc\\Router\\RouteMatch::setMatchedRouteName"),
-      nr_zend2_name_the_wt TSRMLS_CC);
-
-  /*
-   * The functions above were moved to a new package and namespace in
-   * version 3.0.
-   */
   nr_php_wrap_user_function(
       NR_PSTR("Zend\\Router\\HTTP\\RouteMatch::setMatchedRouteName"),
-      nr_zend2_name_the_wt TSRMLS_CC);
+      nr_zend3_name_the_wt TSRMLS_CC);
   nr_php_wrap_user_function(
       NR_PSTR("Zend\\Router\\RouteMatch::setMatchedRouteName"),
-      nr_zend2_name_the_wt TSRMLS_CC);
+      nr_zend3_name_the_wt TSRMLS_CC);
 }
