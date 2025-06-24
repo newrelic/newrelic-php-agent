a31167a397a75fb5cd09c642e9e44249179815ac
Hitesh Ahuja <108540135+hahuja2@users.noreply.github.com>
2025-06-24 09:57:03 -0700

    chore(agent): remove obsolete code (16/17) (#1085)
    This PR removes obsolete `PHP7` codeblocks. PR: 16/17

--- agent/tests/test_fw_codeigniter.c
+++ agent/tests/test_fw_codeigniter.c
@@ -27,7 +27,6 @@ static void invoke_cufa(TSRMLS_D) {
 }
 
 static void test_get_topmost_user_op_array(TSRMLS_D) {
-#ifdef PHP7
   /*
    * First, we'll test this with call_user_func_array() inlining.
    */
@@ -43,11 +42,6 @@ static void test_get_topmost_user_op_array(TSRMLS_D) {
   CG(compiler_options) |= ZEND_COMPILE_NO_BUILTINS;
   invoke_cufa(TSRMLS_C);
   tlib_php_request_end();
-#else
-  tlib_php_request_start();
-  invoke_cufa(TSRMLS_C);
-  tlib_php_request_end();
-#endif /* PHP7 */
 }
 
 tlib_parallel_info_t parallel_info = {.suggested_nthreads = 1, .state_size = 0};
