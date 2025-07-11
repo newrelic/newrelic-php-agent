726fb76db25e505ba800028cd4e04211c93b7369
Brian Duranleau <bduranleau@newrelic.com>
2025-07-11 11:47:27 -0500

    optimize conditionals, wrap key in safestr

--- agent/fw_drupal8.c
+++ agent/fw_drupal8.c
@@ -607,21 +607,21 @@ NR_PHP_WRAPPER_END
 static bool nr_is_invalid_key_val_arr(nr_php_string_hash_key_t* key,
                                       zval* val,
                                       const char* key_ident) {
-  if (NULL == key || 0 == ZEND_STRING_LEN(key)
-      || 0 == nr_php_is_zval_valid_array(val)
-      || 0 == zend_hash_num_elements(Z_ARRVAL_P(val))) {
-    if (NULL != key) {
-      nrl_warning(NRL_FRAMEWORK,
-                  "hookImplementationsMap[%s]: invalid value for key '%s'",
-                  key_ident, ZEND_STRING_VALUE(key));
-    } else {
-      nrl_warning(NRL_FRAMEWORK, "hookImplementationsMap[%s]: invalid key",
-                  key_ident);
-    }
+  if (NULL == key || 0 == ZEND_STRING_LEN(key)) {
+    nrl_warning(NRL_FRAMEWORK, "hookImplementationsMap[%s]: invalid key",
+                key_ident);
     return true;
-  } else {
-    return false;
   }
+
+  if (0 == nr_php_is_zval_valid_array(val)
+      || 0 == zend_hash_num_elements(Z_ARRVAL_P(val))) {
+    nrl_warning(NRL_FRAMEWORK,
+                "hookImplementationsMap[%s]: invalid value for key '%s'",
+                key_ident, NRSAFESTR(ZEND_STRING_VALUE(key)));
+    return true;
+  }
+
+  return false;
 }
 
 /*
@@ -672,7 +672,7 @@ static bool nr_drupal_hook_attribute_instrument(zval* module_handler) {
             nrl_warning(
                 NRL_FRAMEWORK,
                 "hookImplementationsMap[method]: invalid value for key '%s'",
-                ZEND_STRING_VALUE(method_key));
+                NRSAFESTR(ZEND_STRING_VALUE(method_key)));
           } else {
             nrl_warning(NRL_FRAMEWORK,
                         "hookImplementationsMap[method]: invalid key");
