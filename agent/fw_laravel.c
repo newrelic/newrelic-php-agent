6ad94176db9f83f5e1fb20e36b3689eb3ec365ff
Hitesh Ahuja <hahuja@newrelic.com>
2025-10-09 14:54:25 -0700

    testing

--- agent/fw_laravel.c
+++ agent/fw_laravel.c
@@ -1259,6 +1259,10 @@ void nr_laravel_enable(TSRMLS_D) {
   nr_php_wrap_user_function_before_after_clean(
       NR_PSTR("Laravel\\Horizon\\Console\\SupervisorCommand::handle"),
       nr_laravel_horizon_end_txn, NULL, NULL);
+
+  nr_php_wrap_user_function_before_after_clean(
+      NR_PSTR("Illuminate\\Queue\\Worker::daemonShouldRun"),
+      nr_laravel_horizon_end_txn, NULL, NULL);
 #else
   nr_php_wrap_user_function(NR_PSTR("Symfony\\Component\\Console\\Application::doRun"),
                             nr_laravel_console_application_dorun TSRMLS_CC);
