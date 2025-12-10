41d9823b06f58c3e7adca5bda41f3616e537461d
Michael Fulbright <89205663+mfulb@users.noreply.github.com>
2025-12-10 12:30:03 -0500

    Clear cached package info on restart

--- daemon/internal/newrelic/processor.go
+++ daemon/internal/newrelic/processor.go
@@ -886,7 +886,9 @@ func (p *Processor) processHarvestError(d HarvestError) {
 		app.state = AppStateDisconnected
 		p.shutdownAppHarvest(d.id)
 	case d.Reply.IsRestartException() || app.state == AppStateRestart:
+		log.Debugf("restart connection - stats code received = %d app.state = %d", d.Reply.StatusCode, app.state)
 		app.state = AppStateUnknown
+		app.ResetPhpPackages()
 		p.shutdownAppHarvest(d.id)
 		p.considerConnect(app)
 	}
