12e3d865bd4a2a0c2338b1ddcd555fbc0d1c6b52
Brian Duranleau <bduranleau@newrelic.com>
2026-05-14 15:48:55 -0500

    fiber txn global init/free utils

--- agent/php_fibers.c
+++ agent/php_fibers.c
@@ -5,6 +5,7 @@
  * This file handles fibers instrumentation.
  */
 
+#include "nr_mysqli_metadata.h"
 #include "nr_mysqli_metadata_private.h"
 #include "php_agent.h"
 #include "php_newrelic.h"
@@ -35,13 +36,36 @@ static void nrf_txn_global_deep_copy(txn_globals_t* dest, txn_globals_t* src) {
 #undef COPY_HASHMAP
 
 txn_globals_t* nrf_fiber_init_txn_globals() {
-  txn_globals_t* fiber_globals = NULL;
+  txn_globals_t* fiber_txn_globals = NULL;
 
-  fiber_globals = nr_malloc(sizeof(txn_globals_t));
+  fiber_txn_globals = nr_malloc(sizeof(txn_globals_t));
 
-  nrf_txn_global_deep_copy(fiber_globals, &NRPRG(txn_globals));
+  nrf_txn_global_deep_copy(fiber_txn_globals, &NRPRG(txn_globals));
 
-  return fiber_globals;
+  return fiber_txn_globals;
+}
+
+void free_fiber_globals(void* fiber_globals) {
+  fiber_globals_t* f = (fiber_globals_t*)fiber_globals;
+  nr_hashmap_destroy(&f->txn_globals->guzzle_objs);
+  nr_hashmap_destroy(&f->txn_globals->mysqli_queries);
+  nr_hashmap_destroy(&f->txn_globals->pdo_link_options);
+  nr_hashmap_destroy(&f->txn_globals->curl_metadata);
+  nr_hashmap_destroy(&f->txn_globals->curl_multi_metadata);
+  nr_hashmap_destroy(&f->txn_globals->prepared_statements);
+  nr_mysqli_metadata_destroy(&f->txn_globals->mysqli_links);
+}
+
+void nrf_fiber_init_global_hashmap() {
+  if (NULL != NRPRG_SHARED(fiber_globals_map)) {
+    NRPRG_SHARED(fiber_globals_map) = nr_hashmap_create(free_fiber_globals);
+  }
+}
+
+void nrf_fiber_destroy_global_hashmap() {
+  if (NULL != NRPRG_SHARED(fiber_globals_map)) {
+    nr_hashmap_destroy(&NRPRG_SHARED(fiber_globals_map));
+  }
 }
 
 #endif  // PHP 8.1+
