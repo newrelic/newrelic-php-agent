86b38a8151103a06ede2671a30760e18e9cc6857
Brian Duranleau <bduranleau@newrelic.com>
2026-06-08 16:39:05 -0500

    chore: copyright headers

--- agent/php_fibers.c
+++ agent/php_fibers.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 New Relic Corporation. All rights reserved.
+ * Copyright 2026 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * This file handles fibers instrumentation.
