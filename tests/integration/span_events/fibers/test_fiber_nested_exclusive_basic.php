c19b25989236103503aac105beda67185b61a3f1
Amber Sistla <asistla@newrelic.com>
2026-08-07 15:55:56 -0700

    removed debugs

--- tests/integration/span_events/fibers/test_fiber_nested_exclusive_basic.php
+++ tests/integration/span_events/fibers/test_fiber_nested_exclusive_basic.php
@@ -37,7 +37,6 @@ ok - fiber two: total - exclusive diff is as expected
 
 require_once(realpath(dirname(__FILE__)) . '/../../../include/integration.php');
 require_once(realpath(dirname(__FILE__)) . '/../../../include/tap.php');
-require_once(realpath(dirname(__FILE__)) . '/../../../include/helpers.php');
 
 use NewRelic\Integration\Transaction;
 
@@ -49,7 +48,6 @@ if (extension_loaded('newrelic')) { // Ensure PHP agent is available
 function two()
 {
     echo "Starting Func 'two'\n";
-    env_var_for_expects("GUID_TWO", newrelic_get_linking_metadata()['span.id'] ?? '');
     Fiber::suspend();
     Fiber::suspend();
     time_nanosleep(0, 100000000);
@@ -59,7 +57,6 @@ function two()
 function one()
 {
     echo "Starting Func 'one'\n";
-    env_var_for_expects("GUID_ONE", newrelic_get_linking_metadata()['span.id'] ?? '');
     $fiber2 = new Fiber('two');
     $fiber2->start();
         time_nanosleep(0, 100000000);
@@ -80,19 +77,10 @@ $metrics = $txn->getScopedMetrics();
 tap_assert(isset($metrics["Custom/two"]), 'metric for fiber two exists');
 tap_assert(isset($metrics["Custom/one"]), 'metric for fiber one exists');
 
-//echo "one exclusive: " . ($metrics["Custom/one"]->exclusive) . "\n";
-//echo "one total: " . ($metrics["Custom/one"]->total) . "\n";
-//echo "one diff: " . ($metrics["Custom/one"]->total - $metrics["Custom/one"]->exclusive) . "\n";
 // The diff should be approx equal to the time spent suspended(0.3) + the time it's child spent sleeping (0.1)
 $round_one = round($metrics["Custom/one"]->total - $metrics["Custom/one"]->exclusive, 1);
 tap_assert($round_one === 0.3 + 0.1, 'fiber one: total - exclusive diff is as expected');
-//echo $round_one . "\n";
 
-
-//echo "two exclusive: " . ($metrics["Custom/two"]->exclusive) . "\n";
-//echo "two total: " . ($metrics["Custom/two"]->total) . "\n";
-//echo "two diff: " . ($metrics["Custom/two"]->total - $metrics["Custom/two"]->exclusive) . "\n";
 // The diff should be approx equal to the time spent suspended(0.1 + 0.1)
 $round_two = round($metrics["Custom/two"]->total - $metrics["Custom/two"]->exclusive, 1);
 tap_assert($round_two === 0.1 + 0.1, 'fiber two: total - exclusive diff is as expected');
-//echo $round_two . "\n";
